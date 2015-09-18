78ec5615b0e0ff19e94cd63e406f6999c3ee85d7
unknown <quidele@PC0157.LANCENTRAL.AD>
2015-09-18 17:24:15 -0300

    cierre viernes 18-09

---
+++ incidentes/201509/desbloquear_comprobantes.sql
@@ -0,0 +1,47 @@
+
+
+	drop table tmp_desbloquear_viajes_18092015
+
+	select top 1  nrComprabanteCliente , vlMontoCupon , nrLicencia , 'N' encontrado, 0 flCompensado   into tmp_desbloquear_viajes_18092015 from TB_Cupones 
+
+	delete  from tmp_desbloquear_viajes_18092015
+
+	 insert into tmp_desbloquear_viajes_18092015  ( nrComprabanteCliente , vlMontoCupon , nrLicencia , flCompensado , encontrado ) values ( 101333 , 350 , 62 , 0 , 'N' )
+ insert into tmp_desbloquear_viajes_18092015  ( nrComprabanteCliente , vlMontoCupon , nrLicencia , flCompensado , encontrado ) values ( 563272 , 350 , 62 , 0 , 'N' )
+ insert into tmp_desbloquear_viajes_18092015  ( nrComprabanteCliente , vlMontoCupon , nrLicencia , flCompensado , encontrado ) values ( 563782 , 350 , 62 , 0 , 'N' )
+ insert into tmp_desbloquear_viajes_18092015  ( nrComprabanteCliente , vlMontoCupon , nrLicencia , flCompensado , encontrado ) values ( 357368 , 350 , 62 , 0 , 'N' )
+ insert into tmp_desbloquear_viajes_18092015  ( nrComprabanteCliente , vlMontoCupon , nrLicencia , flCompensado , encontrado ) values ( 564546 , 350 , 62 , 0 , 'N' )
+ insert into tmp_desbloquear_viajes_18092015  ( nrComprabanteCliente , vlMontoCupon , nrLicencia , flCompensado , encontrado ) values ( 358262 , 380 , 62 , 0 , 'N' )
+ insert into tmp_desbloquear_viajes_18092015  ( nrComprabanteCliente , vlMontoCupon , nrLicencia , flCompensado , encontrado ) values ( 565591 , 350 , 62 , 0 , 'N' )
+ insert into tmp_desbloquear_viajes_18092015  ( nrComprabanteCliente , vlMontoCupon , nrLicencia , flCompensado , encontrado ) values ( 565735 , 350 , 62 , 0 , 'N' )
+ insert into tmp_desbloquear_viajes_18092015  ( nrComprabanteCliente , vlMontoCupon , nrLicencia , flCompensado , encontrado ) values ( 566008 , 350 , 62 , 0 , 'N' )
+ insert into tmp_desbloquear_viajes_18092015  ( nrComprabanteCliente , vlMontoCupon , nrLicencia , flCompensado , encontrado ) values ( 14154 , 380 , 62 , 0 , 'N' )
+ insert into tmp_desbloquear_viajes_18092015  ( nrComprabanteCliente , vlMontoCupon , nrLicencia , flCompensado , encontrado ) values ( 567010 , 380 , 62 , 0 , 'N' )
+ insert into tmp_desbloquear_viajes_18092015  ( nrComprabanteCliente , vlMontoCupon , nrLicencia , flCompensado , encontrado ) values ( 567400 , 380 , 62 , 0 , 'N' )
+ insert into tmp_desbloquear_viajes_18092015  ( nrComprabanteCliente , vlMontoCupon , nrLicencia , flCompensado , encontrado ) values ( 563044 , 350 , 2 , 0 , 'N' )
+ insert into tmp_desbloquear_viajes_18092015  ( nrComprabanteCliente , vlMontoCupon , nrLicencia , flCompensado , encontrado ) values ( 13747 , 350 , 2 , 0 , 'N' )
+ insert into tmp_desbloquear_viajes_18092015  ( nrComprabanteCliente , vlMontoCupon , nrLicencia , flCompensado , encontrado ) values ( 563590 , 350 , 2 , 0 , 'N' )
+ insert into tmp_desbloquear_viajes_18092015  ( nrComprabanteCliente , vlMontoCupon , nrLicencia , flCompensado , encontrado ) values ( 564415 , 350 , 2 , 0 , 'N' )
+ insert into tmp_desbloquear_viajes_18092015  ( nrComprabanteCliente , vlMontoCupon , nrLicencia , flCompensado , encontrado ) values ( 358878 , 330 , 2 , 0 , 'N' )
+ insert into tmp_desbloquear_viajes_18092015  ( nrComprabanteCliente , vlMontoCupon , nrLicencia , flCompensado , encontrado ) values ( 565306 , 350 , 2 , 0 , 'N' )
+ insert into tmp_desbloquear_viajes_18092015  ( nrComprabanteCliente , vlMontoCupon , nrLicencia , flCompensado , encontrado ) values ( 564787 , 350 , 2 , 0 , 'N' )
+ insert into tmp_desbloquear_viajes_18092015  ( nrComprabanteCliente , vlMontoCupon , nrLicencia , flCompensado , encontrado ) values ( 565908 , 370 , 2 , 0 , 'N' )
+ insert into tmp_desbloquear_viajes_18092015  ( nrComprabanteCliente , vlMontoCupon , nrLicencia , flCompensado , encontrado ) values ( 566331 , 380 , 2 , 0 , 'N' )
+ insert into tmp_desbloquear_viajes_18092015  ( nrComprabanteCliente , vlMontoCupon , nrLicencia , flCompensado , encontrado ) values ( 566900 , 380 , 2 , 0 , 'N' )
+ insert into tmp_desbloquear_viajes_18092015  ( nrComprabanteCliente , vlMontoCupon , nrLicencia , flCompensado , encontrado ) values ( 567326 , 380 , 2 , 0 , 'N' )
+ insert into tmp_desbloquear_viajes_18092015  ( nrComprabanteCliente , vlMontoCupon , nrLicencia , flCompensado , encontrado ) values ( 568170 , 380 , 2 , 0 , 'N' )
+ insert into tmp_desbloquear_viajes_18092015  ( nrComprabanteCliente , vlMontoCupon , nrLicencia , flCompensado , encontrado ) values ( 567798 , 380 , 2 , 0 , 'N' )
+ insert into tmp_desbloquear_viajes_18092015  ( nrComprabanteCliente , vlMontoCupon , nrLicencia , flCompensado , encontrado ) values ( 382079 , 400 , 1 , 0 , 'N' )
+ insert into tmp_desbloquear_viajes_18092015  ( nrComprabanteCliente , vlMontoCupon , nrLicencia , flCompensado , encontrado ) values ( 382016 , 400 , 1 , 0 , 'N' )
+ insert into tmp_desbloquear_viajes_18092015  ( nrComprabanteCliente , vlMontoCupon , nrLicencia , flCompensado , encontrado ) values ( 4523 , 400 , 11 , 0 , 'N' )
+ insert into tmp_desbloquear_viajes_18092015  ( nrComprabanteCliente , vlMontoCupon , nrLicencia , flCompensado , encontrado ) values ( 379273 , 400 , 11 , 0 , 'N' )
+ insert into tmp_desbloquear_viajes_18092015  ( nrComprabanteCliente , vlMontoCupon , nrLicencia , flCompensado , encontrado ) values ( 384501 , 400 , 11 , 0 , 'N' )
+ insert into tmp_desbloquear_viajes_18092015  ( nrComprabanteCliente , vlMontoCupon , nrLicencia , flCompensado , encontrado ) values ( 384028 , 400 , 11 , 0 , 'N' )
+
+
+ select * from tmp_desbloquear_viajes_18092015 x inner join TB_Cupones y 
+							on x.nrComprabanteCliente = y.nrComprabanteCliente and x.vlMontoCupon = y.vlMontoCupon  and x.nrLicencia = y.nrLicencia
+							 
+
+ update x set x.encontrado ='S' , x.flCompensado = y.flCompensado  from tmp_desbloquear_viajes_18092015 x inner join TB_Cupones y 
+							on x.nrComprabanteCliente = y.nrComprabanteCliente and x.vlMontoCupon = y.vlMontoCupon  and x.nrLicencia = y.nrLicencia
