8b1c72d9984e48c44cbb042b9ad6a8f32e6ffe50
quidele <eulisesquidel@gmail.com>
2015-12-31 16:03:18 -0300

    correccion compro dupli fin 2015

---
+++ incidentes/201601/Rv Pisar Programa Para Corregir Error de carga manual/correccion.sql
@@ -0,0 +1,91 @@
+
+
+ delete from tmp_comproDupliFinANIO
+
+insert into tmp_comproDupliFinANIO  select x.* from TB_Comprobantes x inner join tb_cupones y on
+ x.nrComprobante = y.nrComprabanteCliente and x.nrTalonario =  y.nrTalonarioCliente and  x.tpLetra = y.tpLetraCliente 
+ and x.tpComprobante =  y.tpComprobanteCliente    where nrComprobante like '%12873%' and x.nrLicencia = 61 and nrCaja = 3000007220 and flCompensado = 0 
+insert into tmp_comproDupliFinANIO  select x.* from TB_Comprobantes x inner join tb_cupones y on
+ x.nrComprobante = y.nrComprabanteCliente and x.nrTalonario =  y.nrTalonarioCliente and  x.tpLetra = y.tpLetraCliente 
+ and x.tpComprobante =  y.tpComprobanteCliente    where nrComprobante like '%12874%' and x.nrLicencia = 38 and nrCaja = 3000007220 and flCompensado = 0 
+insert into tmp_comproDupliFinANIO  select x.* from TB_Comprobantes x inner join tb_cupones y on
+ x.nrComprobante = y.nrComprabanteCliente and x.nrTalonario =  y.nrTalonarioCliente and  x.tpLetra = y.tpLetraCliente 
+ and x.tpComprobante =  y.tpComprobanteCliente    where nrComprobante like '%12875%' and x.nrLicencia = 71 and nrCaja = 3000007220 and flCompensado = 0 
+insert into tmp_comproDupliFinANIO  select x.* from TB_Comprobantes x inner join tb_cupones y on
+ x.nrComprobante = y.nrComprabanteCliente and x.nrTalonario =  y.nrTalonarioCliente and  x.tpLetra = y.tpLetraCliente 
+ and x.tpComprobante =  y.tpComprobanteCliente    where nrComprobante like '%12877%' and x.nrLicencia = 32 and nrCaja = 3000007220 and flCompensado = 0 
+insert into tmp_comproDupliFinANIO  select x.* from TB_Comprobantes x inner join tb_cupones y on
+ x.nrComprobante = y.nrComprabanteCliente and x.nrTalonario =  y.nrTalonarioCliente and  x.tpLetra = y.tpLetraCliente 
+ and x.tpComprobante =  y.tpComprobanteCliente    where nrComprobante like '%12878%' and x.nrLicencia = 37 and nrCaja = 3000007220 and flCompensado = 0 
+insert into tmp_comproDupliFinANIO  select x.* from TB_Comprobantes x inner join tb_cupones y on
+ x.nrComprobante = y.nrComprabanteCliente and x.nrTalonario =  y.nrTalonarioCliente and  x.tpLetra = y.tpLetraCliente 
+ and x.tpComprobante =  y.tpComprobanteCliente    where nrComprobante like '%12879%' and x.nrLicencia = 97 and nrCaja = 3000007220 and flCompensado = 0 
+insert into tmp_comproDupliFinANIO  select x.* from TB_Comprobantes x inner join tb_cupones y on
+ x.nrComprobante = y.nrComprabanteCliente and x.nrTalonario =  y.nrTalonarioCliente and  x.tpLetra = y.tpLetraCliente 
+ and x.tpComprobante =  y.tpComprobanteCliente    where nrComprobante like '%108474%' and x.nrLicencia = 37 and nrCaja = 3000007220 and flCompensado = 0 
+insert into tmp_comproDupliFinANIO  select x.* from TB_Comprobantes x inner join tb_cupones y on
+ x.nrComprobante = y.nrComprabanteCliente and x.nrTalonario =  y.nrTalonarioCliente and  x.tpLetra = y.tpLetraCliente 
+ and x.tpComprobante =  y.tpComprobanteCliente    where nrComprobante like '%108475%' and x.nrLicencia = 35 and nrCaja = 3000007220 and flCompensado = 0 
+insert into tmp_comproDupliFinANIO  select x.* from TB_Comprobantes x inner join tb_cupones y on
+ x.nrComprobante = y.nrComprabanteCliente and x.nrTalonario =  y.nrTalonarioCliente and  x.tpLetra = y.tpLetraCliente 
+ and x.tpComprobante =  y.tpComprobanteCliente    where nrComprobante like '%108476%' and x.nrLicencia = 40 and nrCaja = 3000007220 and flCompensado = 0 
+insert into tmp_comproDupliFinANIO  select x.* from TB_Comprobantes x inner join tb_cupones y on
+ x.nrComprobante = y.nrComprabanteCliente and x.nrTalonario =  y.nrTalonarioCliente and  x.tpLetra = y.tpLetraCliente 
+ and x.tpComprobante =  y.tpComprobanteCliente    where nrComprobante like '%108477%' and x.nrLicencia = 43 and nrCaja = 3000007220 and flCompensado = 0 
+insert into tmp_comproDupliFinANIO  select x.* from TB_Comprobantes x inner join tb_cupones y on
+ x.nrComprobante = y.nrComprabanteCliente and x.nrTalonario =  y.nrTalonarioCliente and  x.tpLetra = y.tpLetraCliente 
+ and x.tpComprobante =  y.tpComprobanteCliente    where nrComprobante like '%108478%' and x.nrLicencia = 44 and nrCaja = 3000007220 and flCompensado = 0 
+insert into tmp_comproDupliFinANIO  select x.* from TB_Comprobantes x inner join tb_cupones y on
+ x.nrComprobante = y.nrComprabanteCliente and x.nrTalonario =  y.nrTalonarioCliente and  x.tpLetra = y.tpLetraCliente 
+ and x.tpComprobante =  y.tpComprobanteCliente    where nrComprobante like '%108479%' and x.nrLicencia = 45 and nrCaja = 3000007220 and flCompensado = 0 
+insert into tmp_comproDupliFinANIO  select x.* from TB_Comprobantes x inner join tb_cupones y on
+ x.nrComprobante = y.nrComprabanteCliente and x.nrTalonario =  y.nrTalonarioCliente and  x.tpLetra = y.tpLetraCliente 
+ and x.tpComprobante =  y.tpComprobanteCliente    where nrComprobante like '%108480%' and x.nrLicencia = 47 and nrCaja = 3000007220 and flCompensado = 0 
+insert into tmp_comproDupliFinANIO  select x.* from TB_Comprobantes x inner join tb_cupones y on
+ x.nrComprobante = y.nrComprabanteCliente and x.nrTalonario =  y.nrTalonarioCliente and  x.tpLetra = y.tpLetraCliente 
+ and x.tpComprobante =  y.tpComprobanteCliente    where nrComprobante like '%108481%' and x.nrLicencia = 19 and nrCaja = 3000007220 and flCompensado = 0 
+
+
+
+insert into tmp_comproDupliFinANIO  select x.* from TB_Comprobantes x inner join tb_cupones y on
+ x.nrComprobante = y.nrComprabanteCliente and x.nrTalonario =  y.nrTalonarioCliente and  x.tpLetra = y.tpLetraCliente 
+ and x.tpComprobante =  y.tpComprobanteCliente    where nrComprobante like '%12851%' and x.nrLicencia = 45 and nrCaja = 1000010407 and flCompensado = 0 
+insert into tmp_comproDupliFinANIO  select x.* from TB_Comprobantes x inner join tb_cupones y on
+ x.nrComprobante = y.nrComprabanteCliente and x.nrTalonario =  y.nrTalonarioCliente and  x.tpLetra = y.tpLetraCliente 
+ and x.tpComprobante =  y.tpComprobanteCliente    where nrComprobante like '%12852%' and x.nrLicencia = 3 and nrCaja = 1000010407 and flCompensado = 0 
+
+
+ select * from tmp_comproDupliFinANIO
+
+  -- Backup TB_ComprobantesDetalle 
+select y.* into TB_ComprobantesDetalle_dupliFINANIO from tmp_comproDupliFinANIO x inner join TB_ComprobantesDetalle  y on
+ x.nrComprobante = y.nrComprobante and x.nrTalonario =  y.nrTalonario and  x.tpLetra = y.tpLetra 
+ and x.tpComprobante =  y.tpComprobante  
+
+-- Backup Cupones 
+select y.* into tb_cupones_dupliFINANIO from tmp_comproDupliFinANIO x inner join tb_cupones y on
+ x.nrComprobante = y.nrComprabanteCliente and x.nrTalonario =  y.nrTalonarioCliente and  x.tpLetra = y.tpLetraCliente 
+ and x.tpComprobante =  y.tpComprobanteCliente  
+
+ -- Backup TB_Comprobantes 
+select y.* into tb_comprobantes_dupliFINANIO from tmp_comproDupliFinANIO x inner join TB_Comprobantes  y on
+ x.nrComprobante = y.nrComprobante and x.nrTalonario =  y.nrTalonario and  x.tpLetra = y.tpLetra 
+ and x.tpComprobante =  y.tpComprobante  
+
+ 
+
+  -- Backup TB_ComprobantesDetalle 
+delete y  from tmp_comproDupliFinANIO x inner join TB_ComprobantesDetalle  y on
+ x.nrComprobante = y.nrComprobante and x.nrTalonario =  y.nrTalonario and  x.tpLetra = y.tpLetra 
+ and x.tpComprobante =  y.tpComprobante  
+
+-- Backup Cupones 
+delete y  from tmp_comproDupliFinANIO x inner join tb_cupones y on
+ x.nrComprobante = y.nrComprabanteCliente and x.nrTalonario =  y.nrTalonarioCliente and  x.tpLetra = y.tpLetraCliente 
+ and x.tpComprobante =  y.tpComprobanteCliente  
+
+ -- Backup TB_Comprobantes 
+delete y  from tmp_comproDupliFinANIO x inner join TB_Comprobantes  y on
+ x.nrComprobante = y.nrComprobante and x.nrTalonario =  y.nrTalonario and  x.tpLetra = y.tpLetra 
+ and x.tpComprobante =  y.tpComprobante  
+
