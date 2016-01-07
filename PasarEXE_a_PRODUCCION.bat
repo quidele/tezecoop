7e0c343e51abc8534d30c5a9b9891ee8d6bc7dbc
unknown <quidele@PC0157.LANCENTRAL.AD>
2016-01-06 22:42:04 -0300

    mejorando script psaje a produccion

--- PasarEXE_a_PRODUCCION.bat
+++ PasarEXE_a_PRODUCCION.bat
@@ -1,4 +1,5 @@
-cd C:\Windows\sc\sg_git\tezecoop
+xcopy C:\Gestion\GestiondeViajesADM\GestiondeViajes.exe C:\Gestion\GestiondeViajesADM\Versiones_Anteriores /y
+xcopy C:\Gestion\GestiondeViajesFacturacionRETORNO\GestiondeViajes.exe C:\Gestion\GestiondeViajesFacturacionRETORNO\\Versiones_Anteriores  /y
 xcopy C:\Windows\sc\sg_git\tezecoop\GestiondeViajes.exe C:\Gestion\GestiondeViajesADM /y
 xcopy C:\Windows\sc\sg_git\tezecoop\GestiondeViajes.exe C:\Gestion\GestiondeViajesFacturacionRETORNO /y
 REM MD C:\Gestion\GestiondeViajesADM\Componentes
