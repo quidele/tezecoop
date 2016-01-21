c6930dacac2eaf236a88ff9cb50992c3fb42ca59
unknown <quidele@PC0157.LANCENTRAL.AD>
2016-01-21 18:07:26 -0300

    arreglo bat

--- PasarEXE_a_PRODUCCION.bat
+++ PasarEXE_a_PRODUCCION.bat
@@ -1,10 +1,18 @@
-xcopy C:\Gestion\GestiondeViajesADM\GestiondeViajes.exe C:\Gestion\GestiondeViajesADM\Versiones_Anteriores /y
-xcopy C:\Gestion\GestiondeViajesFacturacionRETORNO\GestiondeViajes.exe C:\Gestion\GestiondeViajesFacturacionRETORNO\\Versiones_Anteriores  /y
-xcopy C:\Windows\sc\sg_git\tezecoop\GestiondeViajes.exe C:\Gestion\GestiondeViajesADM /y
-xcopy C:\Windows\sc\sg_git\tezecoop\GestiondeViajes.exe C:\Gestion\GestiondeViajesFacturacionRETORNO /y
+MD C:\Gestion\GestiondeViajesADM\Versiones_Anteriores\21012016
+xcopy C:\Gestion\GestiondeViajesADM\rpt_facturachico_imprime_continuo_v4_7.rpt C:\Gestion\GestiondeViajesADM\Versiones_Anteriores\21012016
+xcopy C:\Gestion\GestiondeViajesADM\GestiondeViajes.exe C:\Gestion\GestiondeViajesADM\Versiones_Anteriores\21012016 /y
+xcopy C:\Gestion\GestiondeViajesFacturacionRETORNO\GestiondeViajes.exe C:\Gestion\GestiondeViajesFacturacionRETORNO\Versiones_Anteriores\21012016  /y
+REM xcopy C:\Windows\sc\sg_git\tezecoop\GestiondeViajes.exe C:\Gestion\GestiondeViajesADM /y
+REM xcopy C:\Windows\sc\sg_git\tezecoop\GestiondeViajes.exe C:\Gestion\GestiondeViajesFacturacionRETORNO /y
+REM xcopy C:\Windows\sc\sg_git\tezecoop\Reportes\prod\rpt_facturachico_imprime_continuo_v4_7.rpt C:\Gestion\GestiondeViajesADM /y
+REM xcopy C:\Windows\sc\sg_git\tezecoop\Reportes\prod\rpt_facturachico_imprime_continuo_v4_7.rpt C:\Gestion\GestiondeViajesFacturacionRETORNO /y
 REM MD C:\Gestion\GestiondeViajesADM\Componentes
 REM MD C:\Gestion\GestiondeViajesFacturacionRETORNO\Componentes
 REM xcopy Componentes  C:\Gestion\GestiondeViajesADM\Componentes /i  /Y 
 REM xcopy Componentes  C:\Gestion\GestiondeViajesFacturacionRETORNO\Componentes /i  /Y 
 REM xcopy C:\Windows\sc\sg_git\tezecoop\InstalarComponentes.bat C:\Gestion\GestiondeViajesADM /y
 REM xcopy C:\Windows\sc\sg_git\tezecoop\InstalarComponentes.bat C:\Gestion\GestiondeViajesFacturacionRETORNO /y
+
+
+
+MD C:\tezeccop\21012016
