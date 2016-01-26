9ae7c20ed96ffda23794464209f3d530ec84bd7c
unknown <quidele@PC0157.LANCENTRAL.AD>
2016-01-26 13:10:28 -0300

    pasaje martes 26-01

--- PasarEXE_a_PRODUCCION.bat
+++ PasarEXE_a_PRODUCCION.bat
@@ -1,9 +1,9 @@
-MD C:\Gestion\GestiondeViajesADM\Versiones_Anteriores\23012016
-xcopy C:\Gestion\GestiondeViajesADM\rpt_facturachico_imprime_continuo_v4_7.rpt C:\Gestion\GestiondeViajesADM\Versiones_Anteriores\23012016
-xcopy C:\Gestion\GestiondeViajesADM\GestiondeViajes.exe C:\Gestion\GestiondeViajesADM\Versiones_Anteriores\23012016 /y
-xcopy C:\Gestion\GestiondeViajesFacturacionRETORNO\GestiondeViajes.exe C:\Gestion\GestiondeViajesFacturacionRETORNO\Versiones_Anteriores\23012016  /y
-xcopy C:\Windows\sc\sg_git\tezecoop\GestiondeViajes.exe C:\Gestion\GestiondeViajesADM /y
-xcopy C:\Windows\sc\sg_git\tezecoop\GestiondeViajes.exe C:\Gestion\GestiondeViajesFacturacionRETORNO /y
+REM MD C:\Gestion\GestiondeViajesADM\Versiones_Anteriores\23012016
+xcopy C:\Gestion\GestiondeViajesADM\rpt_facturachico_imprime_continuo_v4_7.rpt C:\Gestion\GestiondeViajesADM\Versiones_Anteriores\26012016
+REM xcopy C:\Gestion\GestiondeViajesADM\GestiondeViajes.exe C:\Gestion\GestiondeViajesADM\Versiones_Anteriores\23012016 /y
+REM xcopy C:\Gestion\GestiondeViajesFacturacionRETORNO\GestiondeViajes.exe C:\Gestion\GestiondeViajesFacturacionRETORNO\Versiones_Anteriores\23012016  /y
+REM xcopy C:\Windows\sc\sg_git\tezecoop\GestiondeViajes.exe C:\Gestion\GestiondeViajesADM /y
+REM xcopy C:\Windows\sc\sg_git\tezecoop\GestiondeViajes.exe C:\Gestion\GestiondeViajesFacturacionRETORNO /y
 xcopy C:\Windows\sc\sg_git\tezecoop\Reportes\prod\rpt_facturachico_imprime_continuo_v4_7.rpt C:\Gestion\GestiondeViajesADM /y
 xcopy C:\Windows\sc\sg_git\tezecoop\Reportes\prod\rpt_facturachico_imprime_continuo_v4_7.rpt C:\Gestion\GestiondeViajesFacturacionRETORNO /y
 REM MD C:\Gestion\GestiondeViajesADM\Componentes
