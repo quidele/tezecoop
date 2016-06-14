8023f70c3bc539ff32571a279bbbb99592d1f323
Eulises Quidel <eulisesquidel@gmail.com>
2016-06-14 18:02:10 -0300

    avance Diseño_Controles_Usuarios 14-06-2016

---
+++ SGLibrary_COM/Diseño_Controles_Usuarios/prjUserControlLibrary/Enums/BotonesABMForm.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WindowsFormsApplication1.Enums
+{
+
+    using System.ComponentModel;
+
+    /// <summary>
+    /// Enumerator for ABMBotonesForm
+    /// </summary>
+    public enum ABMBotonesForm
+    {
+
+
+        /// <summary>
+        /// ABMBotonesForm - FIND
+        /// </summary>
+        [Description("FIND")]
+        FIND = 1,
+
+        /// <summary>
+        /// ABMBotonesForm - EDIT
+        /// </summary>
+        [Description("EDIT")]
+        EDIT,
+
+        /// <summary>
+        /// ABMBotonesForm - ADD
+        /// </summary>
+        [Description("ADD")]
+        ADD,
+
+        /// <summary>
+        /// ABMBotonesForm - DELETE
+        /// </summary>
+        [Description("DELETE")]
+        DELETE,
+
+        /// <summary>
+        /// ABMBotonesForm - SAVE
+        /// </summary>
+        [Description("SAVE")]
+        SAVE,
+
+        /// <summary>
+        /// ABMBotonesForm - CANCEL
+        /// </summary>
+        [Description("CANCEL")]
+        CANCEL
+    }
+
+    
+}
---
+++ SGLibrary_COM/Diseño_Controles_Usuarios/prjUserControlLibrary/UserControl1.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApplication1
+{
+    public partial class UserControl1 : UserControl
+    {
+        public UserControl1()
+        {
+            InitializeComponent();
+        }
+
+
+        public delegate void ClickDelegateHandler(object sender, EventArgs e);
+        public event ClickDelegateHandler ClickEventDelegateHandler;          
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+          
+            ClickEventDelegateHandler.Invoke(sender,e );
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            ClickEventDelegateHandler.Invoke(sender, e);
+        }
+
+        private void UserControl1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
