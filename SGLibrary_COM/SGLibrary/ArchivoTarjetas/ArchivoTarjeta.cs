9454c02b4864c5c046e5b6ba464116b413212b97
Eulises Quidel <eulisesquidel@gmail.com>
2016-08-23 18:09:19 -0300

    avance inicial procesamiento archivo 1h version 4.9.1

---
+++ SGLibrary_COM/SGLibrary/ArchivoTarjetas/ArchivoTarjeta.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+namespace SGLibrary.ArchivoTarjetas
+{
+    public  class ArchivoTarjeta
+    {
+        // List<string> _contenido;
+
+        public ArchivoTarjeta ()
+	    {
+
+	    }
+
+        public List<string> AbrirArchivo(string pNombreArchivo)
+	    {
+            // abrimos el archivo
+            var reader = new StreamReader(File.OpenRead(pNombreArchivo));
+            List<string> _contenido = new List<string>(); 
+            while (!reader.EndOfStream)
+            {
+                var line = reader.ReadLine();
+                _contenido.Add(line);
+            }
+            return _contenido;
+	    }
+
+        public virtual  void procesarArchivo()
+        {
+            throw new System.InvalidOperationException("El cliente debe implmentar este metodo clase ArchivoTarjeta");
+        }
+
+    }
+}
---
+++ SGLibrary_COM/SGLibrary/ArchivoTarjetas/ArchivoTarjetaVisa.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace SGLibrary.ArchivoTarjetas
+{
+    public class ArchivoTarjetaVisa : ArchivoTarjeta 
+    {
+
+
+        public  override void procesarArchivo()
+        {
+            throw new System.InvalidOperationException("Debe implementar este metodo clase ArchivoTarjeta");    
+        }
+
+    }
+}
