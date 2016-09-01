af1cc76a7a0af130442cd440ee4010e08bd21fdb
Euli <eulisesquidel@hotmail.com>
2016-09-01 07:13:21 -0300

    avance  01/09 conci auto ArchivoTarjetaVisa  0.5 h

--- SGLibrary_COM/SGLibrary/ArchivoTarjetas/ArchivoTarjetaVisa.cs
+++ SGLibrary_COM/SGLibrary/ArchivoTarjetas/ArchivoTarjetaVisa.cs
@@ -7,7 +7,7 @@ namespace SGLibrary.ArchivoTarjetas
 {
     public class ArchivoTarjetaVisa : ArchivoTarjeta 
     {
-        
+        const int Cantidad_Columas_VISA = 11;
 
         public  override void procesarArchivo(String pdsUsuario)
         {
@@ -18,33 +18,35 @@ namespace SGLibrary.ArchivoTarjetas
             this.miArchivoTarjeta.dtproceso = DateTime.Now;
             this.miArchivoTarjeta.nombrearchivo = this.NombreArchivo;
 
+
+        
+
             foreach (var item in this.Contenido)
             {
-
+                TB_ArchivoTarjetaDetalle unTB_ArchivoTarjetaDetalle = new TB_ArchivoTarjetaDetalle();
                 string[] columnas= item.Split(';');
-                if (columnas.Count() == 11)
+                if (columnas.Count() == Cantidad_Columas_VISA)
                 {
                     for (int i = 0; i < columnas.Count(); i++)
                     {
                         switch (i)
                         {
-                            case 0:  // Fecha Presentacion
+                            case 0: unTB_ArchivoTarjetaDetalle.fechaPresentacion = DateTime.Parse(columnas[i]); break; // Fecha Presentacion
                             case 1:  // Lote
-                            case 2:  // Fecha Pago
+                            case 2: unTB_ArchivoTarjetaDetalle.fechaPago = DateTime.Parse(columnas[i]); break; // Fecha Pago
                             case 3:  // Descripcion
-                            case 4:  // Fecha Origen
                             case 5:  // Comprobante
-                            case 6:  // Tarjeta
+                            case 6: unTB_ArchivoTarjetaDetalle.tarjeta = columnas[i].Replace('X',' ').Trim(); break;  // Tarjeta
                             case 7:  // Plan
                             case 8:  // Cuota
                             case 9:  // Moneda
-                            case 10: // Importe
+                            case 10: unTB_ArchivoTarjetaDetalle.importe = Decimal.Parse(columnas[i]); break;  // Importe
                             default:
                                 break;
                         }
                     }
                 }
-                
+                this.miArchivoTarjeta.TB_ArchivoTarjetaDetalle.Add(unTB_ArchivoTarjetaDetalle);
             }
 
             throw new System.InvalidOperationException("Debe implementar este metodo clase ArchivoTarjeta");    
