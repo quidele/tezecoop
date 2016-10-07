d8c635db366aee6f6d611d15d9f407283755b48f
Euli <eulisesquidel@hotmail.com>
2016-10-06 21:52:46 -0300

    carga archivos 0h

---
+++ Version4.9/sp_AFIP.sql
@@ -0,0 +1,138 @@
+
+---  exec  dbo.spu_obtieneDatosCITIVentas_v4_8 2, 2015
+
+alter procedure  [dbo].[spu_obtieneDatosCITIVentas_v4_9](@mes int, @anio int) 
+as
+begin
+
+
+		IF EXISTS (SELECT 1 FROM sys.tables WHERE name = 'RTMP_auxiliarPermisosRenglones')
+					DROP TABLE RTMP_auxiliarPermisosRenglones
+		CREATE TABLE RTMP_auxiliarPermisosRenglones (Renglon VARCHAR(400))
+
+		INSERT INTO RTMP_auxiliarPermisosRenglones (Renglon)
+		/* Select TOP 10  convert(varchar, A.dtComprobante,112) as  fec_comp,  -- Fecha de Comprobante
+				right( replicate('0',3) + dbo.UDF_obtenerCodDOC_CITIT(tpComprobante , tpLetra ),3) as  cod_citi,  -- Tipo de Comprobante
+				right( replicate('0',5) + A.nrTalonario,5)  as  serie_comp,	-- Punto de Venta 
+				right( replicate('0',20) + A.nrComprobante,20 ) as  nro_comp,   -- NUmero de Comprobante}
+				right( replicate('0',20) + A.nrComprobante,20 ) as  nro_comphasta,   -- NUmero de Comprobante hasta
+				case  tpIVA when 'RI'  then '80'  
+							else '99'   END As cod_dgi,
+				case tpIVA when 'RI'   then  replace(A.nrDoc,'-','') 
+							else '00000000000' END As nro_dgi,  --- Código del Documento del Comprador
+				Left (  case tpIVA when 'RI'  then A.dsRazonSocial  
+				else 'Consumidor Final' END   + Replicate(' ',30) , 30)  as nom_tit,  -- Nombre y Apellido del Comprador
+		--vlTotalGeneral,
+		dbo.UDF_obtenerFormatoNumericoAFIP ( vlTotalGeneral, 15, 2)      As imp_total,     --  importe total del operacion
+		dbo.UDF_obtenerFormatoNumericoAFIP (ABS(isnull(A.vlSubtotal,0)),15,2) As imp_iva,				--  importe total conceptos que no integran el neto gravado		
+		dbo.UDF_obtenerFormatoNumericoAFIP (0,15,2) As imp_perc_no_cate,						--  percepcion a no categorizados
+		dbo.UDF_obtenerFormatoNumericoAFIP (0,15,2) As imp_exen,						--  importe operaciones exentas
+		dbo.UDF_obtenerFormatoNumericoAFIP (0,15,2) As imp_perc_ctas ,				--  importe percepciones o pagos a ctas impuestos nacionales
+		dbo.UDF_obtenerFormatoNumericoAFIP (0,15,2) As imp_perc_iibb ,				--  importe percepciones de ingresos brutos
+		dbo.UDF_obtenerFormatoNumericoAFIP (0,15,2) As imp_perc_ip ,					--  importe percepciones de impuestos municipales
+		dbo.UDF_obtenerFormatoNumericoAFIP (0,15,2) As imp_impuestos_internos ,		--  importe impuestos internos
+		'PES'      as cod_moneda,					--  codigo de moneda
+		dbo.UDF_obtenerFormatoNumericoAFIP (1,10,6)	   as tipo_cambio,			        --  tipo de cambio
+		'v'      as cant_alicuotas_iva,			--  cantidad alicuotas iva  
+		dbo.UDF_obtenerFormatoNumericoAFIP (0,15,2)      as otros_tributos,			    --  otros tributos
+		convert(varchar, A.dtComprobante + 10,112)  as fecha_venc -- fecha de vencimiento de pago 
+		FROM TB_Comprobantes A 
+		WHERE month(A.dtComprobante) = @mes  and year(A.dtComprobante) = @anio
+		
+		SELECT  dbo.UDF_obtenerFormatoCUITAFIP(30501032545,20)  
+		*/ 
+
+		Select  convert(varchar, A.dtComprobante,112) -- as  fec_comp,  -- Fecha de Comprobante
+				+ right( replicate('0',3) + dbo.UDF_obtenerCodDOC_CITIT_v4_7(tpComprobante , tpLetra ),3) --  as  cod_citi,  -- Tipo de Comprobante
+				+ dbo.UDF_obtenerFormatoNumericoAFIP_v4_7 ( A.nrTalonario,5,0)  --  as  serie_comp,	-- Punto de Venta 
+				+ dbo.UDF_obtenerFormatoNumericoAFIP_v4_7 (rtrim( A.nrComprobante),20,0 )--  as  nro_comp,   -- NUmero de Comprobante}
+				+ dbo.UDF_obtenerFormatoNumericoAFIP_v4_7 (rtrim(  A.nrComprobante),20,0) -- as  nro_comphasta,   -- NUmero de Comprobante hasta
+				+ case  tpIVA when 'RI'  then (case WHEN dbo.ufn_ValidarCUIT(A.nrDoc)=1 then '80' ELSE '99' END)
+							else (CASE WHEN a.vlTotalGeneral > 999 THEN	'80' ELSE   '99'  END )    END -- As cod_dgi,
+				--, A.nrDoc
+				--, A.vlTotalGeneral, 
+				+  case tpIVA when 'RI'    then (case WHEN dbo.ufn_ValidarCUIT(A.nrDoc)=1 then dbo.UDF_obtenerFormatoCUITAFIP_v4_7(A.nrDoc,20) ELSE REPLICATE('0',20) END)   
+				    else ((CASE WHEN a.vlTotalGeneral > 999 THEN	dbo.UDF_obtenerFormatoCUITAFIP_v4_7(A.nrDoc,20) ELSE    REPLICATE('0',20) END) ) END -- As nro_dgi,  --- Código del Documento del Comprador
+				+ Left (  case tpIVA when 'RI'  then A.dsRazonSocial   
+				else (CASE WHEN a.vlTotalGeneral > 999 THEN	A.dsRazonSocial   ELSE   'Consumidor Final'   END) END   + Replicate(' ',30) , 30) --  as nom_tit,  -- Nombre y Apellido del Comprador
+		--vlTotalGeneral,
+		+ dbo.UDF_obtenerFormatoNumericoAFIP_v4_7 ( vlTotalGeneral, 15, 2)   --    As imp_total,     --  importe total del operacion
+		+ dbo.UDF_obtenerFormatoNumericoAFIP_v4_7 (ABS(isnull(A.vlSubtotal,vlTotalGeneral)),15,2) --  As imp_iva,				--  importe total conceptos que no integran el neto gravado		
+		+ dbo.UDF_obtenerFormatoNumericoAFIP_v4_7 (0,15,2) -- As imp_perc_no_cate,						--  percepcion a no categorizados
+		+ dbo.UDF_obtenerFormatoNumericoAFIP_v4_7 (0,15,2) -- As imp_exen,						--  importe operaciones exentas
+		+ dbo.UDF_obtenerFormatoNumericoAFIP_v4_7 (0,15,2) -- As imp_perc_ctas ,				--  importe percepciones o pagos a ctas impuestos nacionales
+		+ dbo.UDF_obtenerFormatoNumericoAFIP_v4_7 (0,15,2) -- As imp_perc_iibb ,				--  importe percepciones de ingresos brutos
+		+ dbo.UDF_obtenerFormatoNumericoAFIP_v4_7 (0,15,2) -- As imp_perc_ip ,					--  importe percepciones de impuestos municipales
+		+ dbo.UDF_obtenerFormatoNumericoAFIP_v4_7 (0,15,2) -- As imp_impuestos_internos ,		--  importe impuestos internos
+		+ 'PES'      -- as cod_moneda,					--  codigo de moneda
+		+ dbo.UDF_obtenerFormatoNumericoAFIP_v4_7 (1,10,6)	--    as tipo_cambio,			        --  tipo de cambio
+		+ '1'     -- as cant_alicuotas_iva,			--  cantidad alicuotas iva  
+		+ ' '     -- as cant_alicuotas_iva,			--  codigo de operacion
+		+ dbo.UDF_obtenerFormatoNumericoAFIP_v4_7 (0,15,2)     --  as otros_tributos,			    --  otros tributos
+		+ convert(varchar, DATEADD (day, 10,  A.dtComprobante ) ,112) --  as fecha_venc -- fecha de vencimiento de pago 
+		FROM TB_Comprobantes A 
+		WHERE month(A.dtComprobante) = @mes    and year(A.dtComprobante) = @anio 
+		-- AND  (vlTotalGeneral   > 1000 AND  tpIVA <> 'RI') and  dbo.ufn_ValidarCUIT('20-25475222-4')
+		--AND not (vlTotalGeneral   > 1000 AND  tpIVA <> 'RI')  
+		AND tpLetra <>'X' 
+		-- AND a.nrComprobante = '00602178' 
+
+		-- select Renglon  from dbSG2000.dbo.RTMP_auxiliarPermisosRenglones
+
+		declare @nombre_archivo varchar(255)=  'CITIVentas_' + convert(varchar,@anio) + right('0' + convert(varchar,@mes), 2) + '.txt'
+
+		exec  [dbo].[spu_generarArchivo_v4_8] @sql_select = 'select Renglon  from dbSG2000.dbo.RTMP_auxiliarPermisosRenglones' , @nombre_archivo = @nombre_archivo
+
+		return 0; 
+
+
+end 
+
+
+
+go
+
+
+---------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+---  exec  dbo.spu_obtieneDatosCITIVentas_Alicuotas_v4_9 1, 2015
+
+alter procedure  [dbo].[spu_obtieneDatosCITIVentas_Alicuotas_v4_9](@mes int, @anio int) 
+as
+begin
+
+
+		IF EXISTS (SELECT 1 FROM sys.tables WHERE name = 'RTMP_auxiliarPermisosRenglones')
+					DROP TABLE RTMP_auxiliarPermisosRenglones
+		CREATE TABLE RTMP_auxiliarPermisosRenglones (Renglon VARCHAR(400))
+
+		INSERT INTO RTMP_auxiliarPermisosRenglones (Renglon)
+		Select  right( replicate('0',3) + dbo.UDF_obtenerCodDOC_CITIT_v4_7(tpComprobante , tpLetra ),3) + -- as  cod_citi,  -- Tipo de Comprobante
+				right( replicate('0',5) +  rtrim(A.nrTalonario),5) + -- as  serie_comp  ,	-- Punto de Venta 
+				right( replicate('0',20) + rtrim(A.nrComprobante),20 ) + -- as  nro_comp,   -- Numero de Comprobante
+		dbo.UDF_obtenerFormatoNumericoAFIP_v4_7 ( isnull(vlTotalGeneral,0) -  ABS(isnull(A.vlIVA,0)) , 15, 2)  + --     As imp_neto_gravado,     --  importe total del operacion
+		+ (case ABS(isnull(A.vlIVA,0)) when 0
+		 then dbo.UDF_obtenerFormatoNumericoAFIP_v4_7 ( 0 , 4, 0)
+		else dbo.UDF_obtenerFormatoNumericoAFIP_v4_7 ( 4 , 4, 0)  end) +
+		-- dbo.UDF_obtenerFormatoNumericoAFIP_v4_7 ( 4 , 4, 0)    + --  As alicuota_iva,     --  importe total del operacion
+		dbo.UDF_obtenerFormatoNumericoAFIP_v4_7 (ABS(isnull(A.vlIVA,0)),15,2)  -- As impuesto_liquidado				--  importe total conceptos que no integran el neto gravado		
+		FROM TB_Comprobantes A 
+		WHERE month(A.dtComprobante) = @mes    and year(A.dtComprobante) = @anio 
+		AND tpLetra <>'X' 
+
+	
+		-- return;
+	    -- select Renglon  from dbSG2000.dbo.RTMP_auxiliarPermisosRenglones
+
+		declare @nombre_archivo varchar(255)=  'CITIVentas_Alicuotas' + convert(varchar,@anio) + right('0' + convert(varchar,@mes), 2) + '.txt'
+
+		exec  [dbo].[spu_generarArchivo_v4_8] @sql_select = 'select Renglon  from dbSG2000.dbo.RTMP_auxiliarPermisosRenglones' , @nombre_archivo = @nombre_archivo
+
+		return 0; 
+
+
+end 
+
+
+
+
