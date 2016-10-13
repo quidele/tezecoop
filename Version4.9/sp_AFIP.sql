6c77d9143128703dd09a9500ab5f405a544b36aa
Eulises Quidel <eulisesquidel@gmail.com>
2016-10-13 18:09:35 -0300

    agregando archivos errorres carga CITI 0h

--- Version4.9/sp_AFIP.sql
+++ Version4.9/sp_AFIP.sql
@@ -146,6 +146,67 @@ begin
 end 
 
 
+go
+
+
+
+create procedure spu_ControlCITIVentas_v4_9
+as
+begin 
+
+		Select  right( replicate('0',3) + dbo.UDF_obtenerCodDOC_CITIT_v4_7(tpComprobante , tpLetra ),3) + -- as  cod_citi,  -- Tipo de Comprobante
+				right( replicate('0',5) +  rtrim(A.nrTalonario),5) + -- as  serie_comp  ,	-- Punto de Venta 
+				right( replicate('0',20) + rtrim(A.nrComprobante),20 ) + -- as  nro_comp,   -- Numero de Comprobante
+		CASE  ABS(isnull(A.vlIVA,0)) when 0  THEN  dbo.UDF_obtenerFormatoNumericoAFIP_v4_7 ( 0 , 15, 2)  ELSE dbo.UDF_obtenerFormatoNumericoAFIP_v4_7 ( isnull(vlTotalGeneral,0) -  ABS(isnull(A.vlIVA,0)) , 15, 2) END  + --     As imp_neto_gravado,     --  importe total del operacion
+		+ (case ABS(isnull(A.vlIVA,0)) when 0
+		 then dbo.UDF_obtenerFormatoNumericoAFIP_v4_7 ( 3 , 4, 0)
+		else dbo.UDF_obtenerFormatoNumericoAFIP_v4_7 ( 5 , 4, 0)  end) +
+		-- dbo.UDF_obtenerFormatoNumericoAFIP_v4_7 ( 4 , 4, 0)    + --  As alicuota_iva,     --  importe total del operacion
+		dbo.UDF_obtenerFormatoNumericoAFIP_v4_7 (ABS(isnull(A.vlIVA,0)),15,2)  -- As impuesto_liquidado				--  importe total conceptos que no integran el neto gravado		
+		FROM TB_Comprobantes A 
+		WHERE month(A.dtComprobante) = @mes    and year(A.dtComprobante) = @anio 
+		AND tpLetra <>'X' 
+
+
+		Select  convert(varchar, A.dtComprobante,112) -- as  fec_comp,  -- Fecha de Comprobante
+				+ right( replicate('0',3) + dbo.UDF_obtenerCodDOC_CITIT_v4_7(tpComprobante , tpLetra ),3) --  as  cod_citi,  -- Tipo de Comprobante
+				+ dbo.UDF_obtenerFormatoNumericoAFIP_v4_7 ( A.nrTalonario,5,0)  --  as  serie_comp,	-- Punto de Venta 
+				+ dbo.UDF_obtenerFormatoNumericoAFIP_v4_7 (rtrim( A.nrComprobante),20,0 )--  as  nro_comp,   -- NUmero de Comprobante}
+				+ dbo.UDF_obtenerFormatoNumericoAFIP_v4_7 (rtrim(  A.nrComprobante),20,0) -- as  nro_comphasta,   -- NUmero de Comprobante hasta
+				+ case  tpIVA when 'RI'  then (case WHEN dbo.ufn_ValidarCUIT(A.nrDoc)=1 then '80' ELSE '99' END)
+							else (CASE WHEN a.vlTotalGeneral > 999 THEN	'80' ELSE   '99'  END )    END -- As cod_dgi,
+				--, A.nrDoc
+				--, A.vlTotalGeneral, 
+				+  case tpIVA when 'RI'    then (case WHEN dbo.ufn_ValidarCUIT(A.nrDoc)=1 then dbo.UDF_obtenerFormatoCUITAFIP_v4_7(A.nrDoc,20) ELSE REPLICATE('0',20) END)   
+				    else ((CASE WHEN a.vlTotalGeneral > 999 THEN	dbo.UDF_obtenerFormatoCUITAFIP_v4_7(A.nrDoc,20) ELSE    REPLICATE('0',20) END) ) END -- As nro_dgi,  --- Código del Documento del Comprador
+				+ Left (  case tpIVA when 'RI'  then A.dsRazonSocial   
+				else (CASE WHEN a.vlTotalGeneral > 999 THEN	A.dsRazonSocial   ELSE   'Consumidor Final'   END) END   + Replicate(' ',30) , 30) --  as nom_tit,  -- Nombre y Apellido del Comprador
+		--vlTotalGeneral,
+		+ dbo.UDF_obtenerFormatoNumericoAFIP_v4_7 (vlTotalGeneral, 15, 2)   --    As imp_total,     --  importe total del operacion
+		+ CASE isnull(a.vlIVA,0) WHEN 0 THEN dbo.UDF_obtenerFormatoNumericoAFIP_v4_7 (vlTotalGeneral,15,2) ELSE dbo.UDF_obtenerFormatoNumericoAFIP_v4_7 (0 ,15,2) END --  ,	--  importe total conceptos que no integran el neto gravado		
+		+ dbo.UDF_obtenerFormatoNumericoAFIP_v4_7 (0,15,2) -- As imp_perc_no_cate,						--  percepcion a no categorizados
+		+ dbo.UDF_obtenerFormatoNumericoAFIP_v4_7 (0,15,2) -- As imp_exen,						--  importe operaciones exentas
+		+ dbo.UDF_obtenerFormatoNumericoAFIP_v4_7 (0,15,2) -- As imp_perc_ctas ,				--  importe percepciones o pagos a ctas impuestos nacionales
+		+ dbo.UDF_obtenerFormatoNumericoAFIP_v4_7 (0,15,2) -- As imp_perc_iibb ,				--  importe percepciones de ingresos brutos
+		+ dbo.UDF_obtenerFormatoNumericoAFIP_v4_7 (0,15,2) -- As imp_perc_ip ,					--  importe percepciones de impuestos municipales
+		+ dbo.UDF_obtenerFormatoNumericoAFIP_v4_7 (0,15,2) -- As imp_impuestos_internos ,		--  importe impuestos internos
+		+ 'PES'      -- as cod_moneda,					--  codigo de moneda
+		+ dbo.UDF_obtenerFormatoNumericoAFIP_v4_7 (1,10,6)	--    as tipo_cambio,			        --  tipo de cambio
+		+ '1'     -- as cant_alicuotas_iva,			--  cantidad alicuotas iva  
+		+ case isnull(a.vlIVA,0) WHEN 0 THEN 'N' ELSE ' '   End  -- as codigo de operacion,			--  codigo de operacion
+		+ dbo.UDF_obtenerFormatoNumericoAFIP_v4_7 (0,15,2)     --  as otros_tributos,			    --  otros tributos
+		+ convert(varchar, DATEADD (day, 10,  A.dtComprobante ) ,112) --  as fecha_venc -- fecha de vencimiento de pago 
+		FROM TB_Comprobantes A 
+		WHERE month(A.dtComprobante) = @mes    and year(A.dtComprobante) = @anio 
+		-- AND  (vlTotalGeneral   > 1000 AND  tpIVA <> 'RI') and  dbo.ufn_ValidarCUIT('20-25475222-4')
+		--AND not (vlTotalGeneral   > 1000 AND  tpIVA <> 'RI')  
+		AND tpLetra <>'X' 
+
+	return;
+end 
+
+
+
 
 go
 
