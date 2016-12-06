661efad69d76cb76c811935cbe4514f680bd759e
Eulises Quidel <eulisesquidel@gmail.com>
2016-12-06 13:21:03 -0300

    Lista de viajes no compensados de 2015

---
+++ incidentes/201612/bajadaViajesNoCompensados2015.sql
@@ -0,0 +1,35 @@
+  use dbSG2000
+  go
+  
+  
+  set dateformat dmy 
+
+  select  a.nrLicencia, a.nmLicenciatario,
+          a.tpCupon, a.nrTalonarioCliente, a.nrComprabanteCliente, a.vlMontoCupon,  convert(varchar,a.dtCupon,103) as fecha, a.dsObservacion  from tb_cupones a
+  where   a.flCompensado = 0  and a.flAnulado = 0 and   nrLicencia not in (999,900) and  
+		  a.dtCupon >= '01/01/2009 00:00:00.000'  and 
+		  a.dtCupon <= '01/01/2015 23:59:59.000'  and 
+		  fleliminar =0
+	
+  use dbSG2000_Pruebas 
+  go
+    
+  set dateformat dmy 
+  		   
+  begin tran
+  		  
+  update a
+  set     flAnulado = 1	,	
+  		  flNoCompensado = 1,
+		  dtNoCompensado = GETDATE()    
+  from tb_cupones a
+  where   a.flCompensado = 0  and a.flAnulado = 0 and   nrLicencia not in (999,900) and  
+		  a.dtCupon >= '01/01/2009 00:00:00.000'  and 
+		  a.dtCupon <= '20/06/2011 23:59:59.000'  and 
+		  fleliminar = 0
+
+ rollback tran
+ 
+ commit tran
+ 
+		  
