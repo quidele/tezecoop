49b7e88c463b51b43e39eb182a00bed341aa92b4
Euli <eulisesquidel@hotmail.com>
2017-01-19 07:06:44 -0300

    19/01 tratando de detectar error validacion error .5h

--- SGLibrary_COM/SGLibrary/ServiceConciliacionManual.cs
+++ SGLibrary_COM/SGLibrary/ServiceConciliacionManual.cs
@@ -232,6 +232,9 @@ namespace SGLibrary
         {
             ServiceMovimientoContable unSMC = new ServiceMovimientoContable();
 
+            try
+            {
+
             using (var context = new dbSG2000Entities())
             {
 
@@ -290,6 +293,26 @@ namespace SGLibrary
 
                 }
 
+            }
+
+            }
+            catch (DbEntityValidationException e)
+            {
+                foreach (var eve in e.EntityValidationErrors)
+            {
+                Console.WriteLine("Entity of type \"{0}\" in state \"{1}\" has the following validation errors:",
+                    eve.Entry.Entity.GetType().Name, eve.Entry.State);
+                Trace.TraceError("Entity of type \"{0}\" in state \"{1}\" has the following validation errors:",
+                    eve.Entry.Entity.GetType().Name, eve.Entry.State);
+                foreach (var ve in eve.ValidationErrors)
+                {
+                    Console.WriteLine("- Property: \"{0}\", Error: \"{1}\"",
+                        ve.PropertyName, ve.ErrorMessage);
+                    Trace.TraceError("- Property: \"{0}\", Error: \"{1}\"",
+                      ve.PropertyName, ve.ErrorMessage);
+                }
+            }
+            throw;
             }
 
 
