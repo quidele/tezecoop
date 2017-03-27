f952e4dbdf6a3d96684779bae6137519606872e1
Eulises Quidel <eulisesquidel@gmail.com>
2017-03-27 18:21:31 -0300

    cierre periodo 201611 enviar mail al contador  correcciones 1h

--- Version4.9.4/ScriptBD_DDL_MSSQL_v4.9.4.sql
+++ Version4.9.4/ScriptBD_DDL_MSSQL_v4.9.4.sql
@@ -130,14 +130,14 @@ END
 Go
 
 
-if exists (SELECT * FROM  INFORMATION_SCHEMA.ROUTINES where SPECIFIC_NAME ='UDF_obtenerCampoAFIP_cod_dgi_v4_9' )
-	drop function  dbo.UDF_obtenerCampoAFIP_cod_dgi_v4_9
+if exists (SELECT * FROM  INFORMATION_SCHEMA.ROUTINES where SPECIFIC_NAME ='UDF_obtenerCampoAFIP_cod_dgi_v4_9_4' )
+	drop function  dbo.UDF_obtenerCampoAFIP_cod_dgi_v4_9_4
 
 GO
 
 
 --  select   [dbo].[UDF_obtenerCampoAFIP_cod_dgi_v4_9] ('CF' , '20100177341         ','580')
-CREATE  FUNCTION [dbo].[UDF_obtenerCampoAFIP_cod_dgi_v4_9] (@tpIVA  varchar(4) ,  @nrDoc as varchar(100), @vlTotalGeneral as float  )
+CREATE  FUNCTION [dbo].[UDF_obtenerCampoAFIP_cod_dgi_v4_9_4] (@tpIVA  varchar(4) ,  @nrDoc as varchar(100), @vlTotalGeneral as float  )
 RETURNS varchar(50)
 BEGIN
 declare @valor_retorno char(2) = '99' 
@@ -148,8 +148,9 @@ BEGIN
 	IF  dbo.ufn_ValidarCUIT_v4_9_4(@nrDoc)=1
 		set @valor_retorno = '80'
 	ELSE
-			set @valor_retorno = '90'  -- VERIFICAR ESTA SITUACION
+			set @valor_retorno = '80'  -- VERIFICAR ESTA SITUACION
 END
+ELSE
 BEGIN
 	IF ISNULL(rtrim(@nrDoc),'') = ''
 	BEGIN
@@ -209,7 +210,7 @@ declare @cod_dgi char(2) =''
 
 select @nrDoc = replace(replace(rtrim(@nrDoc),'.',''),'-','')
 
-select @cod_dgi = [dbo].[UDF_obtenerCampoAFIP_cod_dgi_v4_9] (@tpIVA, @nrDoc,@vlTotalGeneral) 
+select @cod_dgi = [dbo].[UDF_obtenerCampoAFIP_cod_dgi_v4_9_4] (@tpIVA, @nrDoc,@vlTotalGeneral) 
 
 IF @cod_dgi = '90'
 BEGIN
@@ -232,6 +233,7 @@ BEGIN
 	ELSE
 		set @valor_retorno =  dbo.UDF_obtenerFormatoCUITAFIP_v4_7('20254752224',20)   -- VERIFICAR ESTA SITUACION
 END
+ELSE
 BEGIN
 	IF @vlTotalGeneral > 999  
 	BEGIN
@@ -247,7 +249,6 @@ BEGIN
 	BEGIN
 		set @valor_retorno =  dbo.UDF_obtenerFormatoCUITAFIP_v4_7(@nrDoc,20) 
 	END
-
 END
 
 
@@ -327,6 +328,38 @@ select * from tb_puestos
  exec  [dbo].[spu_obtieneDatosCITIVentas_v4_9_4] @mes = 1 , @anio = 2017 , @renglon = -1
  exec  [dbo].[spu_obtieneDatosCITIVentas_v4_9_4] @mes = 10 , @anio = 2016 , @renglon = -1
 
+ exec  [dbo].[spu_obtieneDatosCITIVentas_v4_9_4] @mes = 11 , @anio = 2016 , @renglon = 9910
+ exec  [dbo].[spu_obtieneDatosCITIVentas_v4_9_4] @mes = 11 , @anio = 2016 , @renglon = 10335
+ exec  [dbo].[spu_obtieneDatosCITIVentas_v4_9_4] @mes = 11 , @anio = 2016 , @renglon = 14397
+ exec  [dbo].[spu_obtieneDatosCITIVentas_v4_9_4] @mes = 11 , @anio = 2016 , @renglon = 16913
+ exec  [dbo].[spu_obtieneDatosCITIVentas_v4_9_4] @mes = 11 , @anio = 2016 , @renglon = 16914
+ 
+
+ select * from zTB_Comprobantes_201611_corrige_errores 
+
+ drop table zTB_Comprobantes_201611_corrige_errores 
+
+ select '0007' as nrTalonario , 	'00494989' as nrComprobante, 	'B' as tpLetra  , 	'FA' as tpComprobante  into zTB_Comprobantes_201611_corrige_errores
+
+ insert into zTB_Comprobantes_201611_corrige_errores
+ select '0007' as nrTalonario , 	'00494699' as nrComprobante, 	'B' as tpLetra  , 	'FA' as tpComprobante 
+ 
+ insert into zTB_Comprobantes_201611_corrige_errores
+ select '0006' as nrTalonario , 	'00713621' as nrComprobante, 	'B' as tpLetra  , 	'FA' as tpComprobante 
+ 
+ insert into zTB_Comprobantes_201611_corrige_errores
+ select '0006' as nrTalonario , 	'00712711' as nrComprobante, 	'B' as tpLetra  , 	'FA' as tpComprobante 
+ 
+ insert into zTB_Comprobantes_201611_corrige_errores
+ select '0007' as nrTalonario , 	'00491728' as nrComprobante, 	'B' as tpLetra  , 	'FA' as tpComprobante 
+ 
+
+	select x.tpIVA , x.nrDOC , x.dsRazonsocial  , x.vlTotalGeneral,  * from tb_comprobantes  x inner join  zTB_Comprobantes_201611_corrige_errores y
+							on  x.nrTalonario = y.nrTalonario
+							and  x.nrComprobante = y.nrComprobante
+							and  x.tpLetra = y.tpLetra
+							and  x.tpComprobante = y.tpComprobante
+	 
 */
 
 
@@ -385,7 +418,7 @@ declare @nro_linea decimal(18,0) = 0
 				+ dbo.UDF_obtenerFormatoNumericoAFIP_v4_7 ( A.nrTalonario,5,0)  --  as  serie_comp,	-- Punto de Venta 
 				+ dbo.UDF_obtenerFormatoNumericoAFIP_v4_7 (rtrim( A.nrComprobante),20,0 )--  as  nro_comp,   -- NUmero de Comprobante}
 				+ dbo.UDF_obtenerFormatoNumericoAFIP_v4_7 (rtrim(  A.nrComprobante),20,0) -- as  nro_comphasta,   -- NUmero de Comprobante hasta
-				+ [dbo].[UDF_obtenerCampoAFIP_cod_dgi_v4_9](A.tpIVA, A.nrDoc , A.vlTotalGeneral ) -- As cod_dgi,  --- Código del Documento del Comprador
+				+ [dbo].UDF_obtenerCampoAFIP_cod_dgi_v4_9_4(A.tpIVA, A.nrDoc , A.vlTotalGeneral ) -- As cod_dgi,  --- Código del Documento del Comprador
 				-- +'EULISES'+
 				+ [dbo].UDF_obtenerCampoAFIP_nro_dgi_v4_9_4 (A.tpIVA, A.nrDoc , A.vlTotalGeneral ) -- As nro_dgi,  --- Documento del Comprador
 				+ Left (CASE tpIVA when 'RI'  then A.dsRazonSocial   ELSE (CASE WHEN a.vlTotalGeneral > 999 THEN	A.dsRazonSocial   
@@ -639,7 +672,7 @@ GO
 
 
 
-
+/*
  exec  [dbo].[spu_obtieneDatosCITIVentas_v4_9_4] @mes = 11 , @anio = 2016 , @renglon = 12812
 
  30-63020908-7 ? CUIT Online
@@ -650,31 +683,144 @@ GO
  select tpIVA, nrDoc , vlTotalGeneral, dsRazonSocial, * from TB_Comprobantes 
  where nrTalonario = '0006' and  nrComprobante = '00008329' and tpIVA = 'CF'
 
-
  sp_helptext '[dbo].[spu_obtieneDatosCITIVentas_v4_9_4]'
 
  update x set nrDoc = '20124651809' from TB_Comprobantes x 
    where nrTalonario = '0006' and  nrComprobante = '00008365' and tpIVA = 'RI'
 
-   RODRIGUEZ JOSE MARIA
-
-
     update  x set  dsRazonSocial = 'Eulises Quidel' from TB_Comprobantes  x 
  where nrTalonario = '0006' and  nrComprobante = '00117198' and tpIVA = 'CF'
 
 
-
-
     exec  dbo.spu_obtenerUltNroCAIsUsados 11, 2016
-
 
 	 select tpIVA, nrDoc , vlTotalGeneral, dsRazonSocial,* from TB_Comprobantes 
  where tpLetra in ('A') and nrTalonario = '0006'  and nrComprobante  in ('00008329') 
- 
  
   
 	 update  x  set   nrDoc = '30710440413'  from TB_Comprobantes  x 
  where tpLetra in ('A') and nrTalonario = '0006'  and nrComprobante  in ('00008329')       
 
    select dbo.ufn_ValidarCUIT_v4_9_4('30710440413        ')
+
+
+9910	NULL	0008	00170091    	F	FA
+10335	201611130060000300000000000000117198000000000000001171989000000000099999999999*                             000000000103000000000000103000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000PES00010000001N00000000000000020161123	0003	00117198    	B	FA
+14397	201611060010000600000000000000008309000000000000000083098000000000099999999999GEO CONSULT BUENOS AIRES      000000000058000000000000058000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000PES00010000001N00000000000000020161116	0006	00008309    	A	FA
+16913	201611010010000600000000000000008284000000000000000082848000000000099999999999GEO CONSULT BUENOS AIRES      000000000048000000000000048000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000PES00010000001N00000000000000020161111	0006	00008284    	A	FA
+16914	201611010010000600000000000000008285000000000000000082858000000000099999999999GEO CONSULT BUENOS AIRES      000000000048000000000000048000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000PES00010000001N00000000000000020161111	0006	00008285    	A	FA
+
+
+9910	0008	00170091    	F	FA
+10335	0003	00117198    	B	FA
+14397	0006	00008309    	A	FA
+16913	0006	00008284    	A	FA
+16914	0006	00008285    	A	FA
+
+ select * from zTB_Comprobantes_201611_corrige_errores 
+
+ drop table zTB_Comprobantes_201611_corrige_errores 
+
+ select '0008' as nrTalonario , 	'00170091' as nrComprobante, 	'F' as tpLetra  , 	'FA' as tpComprobante  into zTB_Comprobantes_201611_corrige_errores
+
+ insert into zTB_Comprobantes_201611_corrige_errores
+ select '0003' as nrTalonario , 	'00117198' as nrComprobante, 	'B' as tpLetra  , 	'FA' as tpComprobante 
+ 
+ insert into zTB_Comprobantes_201611_corrige_errores
+ select '0006' as nrTalonario , 	'00008309' as nrComprobante, 	'A' as tpLetra  , 	'FA' as tpComprobante 
+ 
+ insert into zTB_Comprobantes_201611_corrige_errores
+ select '0006' as nrTalonario , 	'00008284' as nrComprobante, 	'A' as tpLetra  , 	'FA' as tpComprobante 
+ 
+ insert into zTB_Comprobantes_201611_corrige_errores
+ select '0006' as nrTalonario , 	'00008285' as nrComprobante, 	'A' as tpLetra  , 	'FA' as tpComprobante 
+ 
+
+select x.tpIVA , x.nrDOC , x.dsRazonsocial  , x.vlTotalGeneral,  * from tb_comprobantes  x inner join  zTB_Comprobantes_201611_corrige_errores y
+						on  x.nrTalonario = y.nrTalonario
+						and  x.nrComprobante = y.nrComprobante
+						and  x.tpLetra = y.tpLetra
+						and  x.tpComprobante = y.tpComprobante
+
+
+*/
+
+
+
+
+ drop table zTB_Comprobantes_201611_corrige_errores 
+
+ select '0008' as nrTalonario , 	'00170091' as nrComprobante, 	'F' as tpLetra  , 	'FA' as tpComprobante  into zTB_Comprobantes_201611_corrige_errores
+
+ insert into zTB_Comprobantes_201611_corrige_errores
+ select '0003' as nrTalonario , 	'00117198' as nrComprobante, 	'B' as tpLetra  , 	'FA' as tpComprobante 
+ 
+ insert into zTB_Comprobantes_201611_corrige_errores
+ select '0006' as nrTalonario , 	'00008309' as nrComprobante, 	'A' as tpLetra  , 	'FA' as tpComprobante 
+ 
+ insert into zTB_Comprobantes_201611_corrige_errores
+ select '0006' as nrTalonario , 	'00008284' as nrComprobante, 	'A' as tpLetra  , 	'FA' as tpComprobante 
+ 
+ insert into zTB_Comprobantes_201611_corrige_errores
+ select '0006' as nrTalonario , 	'00008285' as nrComprobante, 	'A' as tpLetra  , 	'FA' as tpComprobante 
+ 
+  select * from zTB_Comprobantes_201611_corrige_errores 
+
+
+select x.tpIVA , x.nrDOC , x.dsRazonsocial  , x.vlTotalGeneral,  * from tb_comprobantes  x inner join  zTB_Comprobantes_201611_corrige_errores y
+						on  x.nrTalonario = y.nrTalonario
+						and  x.nrComprobante = y.nrComprobante
+						and  x.tpLetra = y.tpLetra
+						and  x.tpComprobante = y.tpComprobante
+
+
+
+select * from  TB_Comprobantes  where  nrTalonario =  '0008'  
+											and  nrComprobante = '00170091'  
+											and tpLetra = 'F'	 
+											and  tpComprobante =  'FA'
+
+
+exec  [dbo].[SP_ActualizarComprobante_v4_9]
+		@nrTalonario_param        =  '0008'   , 
+		@nrComprobante_param      =  '00170091'  ,
+		@tpComprobante_param      =  'FA'	,
+		@tpLetra_param            = 'F'	 ,
+		@nrTalonario_new_param    = '0008' ,
+		@nrComprobante_new_param  =  '00170091'  ,
+		@tpComprobante_new_param  =  'FA'	,
+		@tpLetra_new_param        = 'B'	 ,
+		@solo_cambio_cabecera_comprobante = 'SI'
+
+
+
+select x.tpIVA , x.nrDOC , x.dsRazonsocial  , x.vlTotalGeneral, * from  TB_Comprobantes x  where  nrTalonario =  '0003'  
+											and  nrComprobante = '00117198    '  
+											and tpLetra = 'B'	 
+											and  tpComprobante =  'FA'
+
+
+
+
+update x  set  x.nrDOC = null , x.dsRazonsocial = null  from  TB_Comprobantes x  where  nrTalonario =  '0003'  
+											and  nrComprobante = '00117198    '  
+											and tpLetra = 'B'	 
+											and  tpComprobante =  'FA'
+
+
+
+select x.tpIVA , x.nrDOC , x.dsRazonsocial  , x.vlTotalGeneral,  * from tb_comprobantes  x inner join  zTB_Comprobantes_201611_corrige_errores y
+						on  x.nrTalonario = y.nrTalonario
+						and  x.nrComprobante = y.nrComprobante
+						and  x.tpLetra = y.tpLetra
+						and  x.tpComprobante = y.tpComprobante
+						where  x.nrDOC = '3071044041/3        '
+
+
+update x set  x.nrDOC = '30710440413'  from tb_comprobantes  x inner join  zTB_Comprobantes_201611_corrige_errores y
+						on  x.nrTalonario = y.nrTalonario
+						and  x.nrComprobante = y.nrComprobante
+						and  x.tpLetra = y.tpLetra
+						and  x.tpComprobante = y.tpComprobante
+						where  x.nrDOC = '3071044041/3        '
 
