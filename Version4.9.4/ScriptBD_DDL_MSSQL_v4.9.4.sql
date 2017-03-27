4555c9dc3a83c40ba042b78f4b9ae9e5b23f75a3
Euli <eulisesquidel@hotmail.com>
2017-03-27 06:21:44 -0300

    carga archivos del sabado 0h

--- Version4.9.4/ScriptBD_DDL_MSSQL_v4.9.4.sql
+++ Version4.9.4/ScriptBD_DDL_MSSQL_v4.9.4.sql
@@ -636,3 +636,45 @@ end
 
 GO
 
+
+
+
+
+ exec  [dbo].[spu_obtieneDatosCITIVentas_v4_9_4] @mes = 11 , @anio = 2016 , @renglon = 12812
+
+ 30-63020908-7 ? CUIT Online
+
+  select dbo.ufn_ValidarCUIT_v4_9_4('2022611521')
+ select   [dbo].[UDF_obtenerCampoAFIP_cod_dgi_v4_9_4] ('RI' , '306302209087        ','710')
+
+ select tpIVA, nrDoc , vlTotalGeneral, dsRazonSocial, * from TB_Comprobantes 
+ where nrTalonario = '0006' and  nrComprobante = '00008329' and tpIVA = 'CF'
+
+
+ sp_helptext '[dbo].[spu_obtieneDatosCITIVentas_v4_9_4]'
+
+ update x set nrDoc = '20124651809' from TB_Comprobantes x 
+   where nrTalonario = '0006' and  nrComprobante = '00008365' and tpIVA = 'RI'
+
+   RODRIGUEZ JOSE MARIA
+
+
+    update  x set  dsRazonSocial = 'Eulises Quidel' from TB_Comprobantes  x 
+ where nrTalonario = '0006' and  nrComprobante = '00117198' and tpIVA = 'CF'
+
+
+
+
+    exec  dbo.spu_obtenerUltNroCAIsUsados 11, 2016
+
+
+	 select tpIVA, nrDoc , vlTotalGeneral, dsRazonSocial,* from TB_Comprobantes 
+ where tpLetra in ('A') and nrTalonario = '0006'  and nrComprobante  in ('00008329') 
+ 
+ 
+  
+	 update  x  set   nrDoc = '30710440413'  from TB_Comprobantes  x 
+ where tpLetra in ('A') and nrTalonario = '0006'  and nrComprobante  in ('00008329')       
+
+   select dbo.ufn_ValidarCUIT_v4_9_4('30710440413        ')
+
