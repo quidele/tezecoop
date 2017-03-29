caac29000b40a59ea25a87196906ea40d226fe1e
Euli <eulisesquidel@hotmail.com>
2017-03-29 06:38:33 -0300

    iniciando correccion del periodo 201701 0.5h

---
+++ incidentes/201703/arreglos_201701.sql
@@ -0,0 +1,209 @@
+
+
+
+ exec  [dbo].[spu_obtieneDatosCITIVentas_v4_9_4] @mes = 12 , @anio = 2016 , @renglon = 101
+ exec  [dbo].[spu_obtieneDatosCITIVentas_v4_9_4] @mes = 12 , @anio = 2016 , @renglon = 1325
+
+
+  select tpIVA, nrDoc , vlTotalGeneral, dsRazonSocial, * from TB_Comprobantes 
+ where nrTalonario = '0006' and  nrComprobante = '00008565' and  tpLetra ='A' and
+   tpComprobante = 'FA'
+
+ 0006	00008565    	A	FA
+
+
+ select tpIVA, nrDoc , vlTotalGeneral, dsRazonSocial, * 
+       from TB_Comprobantes where dsRazonSocial like 'GODALIA%'
+
+
+
+ select tpIVA, nrDoc , vlTotalGeneral, dsRazonSocial,  dbo.ufn_ValidarCUIT_v4_9_4 (nrDoc) , * 
+       from TB_Comprobantes where dsRazonSocial like 'GODALIA%'  and tpIVA = 'RI' and tpLetra = 'A' 
+							and year(dtComprobante) =  2016
+
+
+	   
+update  x set  nrDoc = '20254752224'
+       from TB_Comprobantes x  where dsRazonSocial like 'GODALIA%'
+
+
+
+ exec  [dbo].[spu_obtieneDatosCITIVentas_v4_9_4] @mes = 12 , @anio = 2016 , @renglon = 1325
+
+ 0002	00012959    	M	FA
+
+   select tpIVA, nrDoc , vlTotalGeneral, dsRazonSocial, * from TB_Comprobantes 
+ where nrTalonario = '0002' and  nrComprobante = '00012959' and  tpLetra ='M' and
+   tpComprobante = 'FA'
+    
+ 
+ select tpIVA, nrDoc , vlTotalGeneral, dsRazonSocial, * from TB_Comprobantes 
+	where tpLetra ='M' and  tpComprobante = 'FA'  and year(dtComprobante) = 2016 and month(dtComprobante) = 12
+
+
+	select dbo.ufn_ValidarCUIT_v4_9_4 (nrDoc) as cuitok, tpIVA, nrDoc , vlTotalGeneral, dsRazonSocial from TB_Comprobantes 
+	where tpLetra  in ('A','M') and  tpComprobante = 'FA'  and year(dtComprobante) = 2016 and month(dtComprobante) = 12
+
+
+		select dbo.ufn_ValidarCUIT_v4_9_4 (nrDoc) as cuitok, tpIVA, nrDoc , vlTotalGeneral, dsRazonSocial from TB_Comprobantes 
+ where nrTalonario = '0009' and  nrComprobante = '00034582' and  tpLetra ='B' and
+   tpComprobante = 'FA'
+
+
+   update x set  x.nrDoc = '30-70704639-9' from TB_Comprobantes x 
+ where nrTalonario = '0009' and  nrComprobante = '00034582' and  tpLetra ='B' and
+   tpComprobante = 'FA'
+
+   	    		
+
+
+	
+
+
+	select * into zTB_Comprobantes_errores_originales_201612 from TB_Comprobantes 
+	where tpLetra  in ('A','M') and  tpComprobante = 'FA'  and year(dtComprobante) = 2016 and 
+	dbo.ufn_ValidarCUIT_v4_9_4 (nrDoc) = 0
+
+
+
+	select dbo.ufn_ValidarCUIT_v4_9_4 (nrDoc) as cuitok, 
+	tpIVA, nrDoc , vlTotalGeneral, dsRazonSocial,
+	nrTalonario, nrComprobante  ,   tpLetra , tpComprobante from TB_Comprobantes 
+	where tpLetra  in ('A','M') and  tpComprobante = 'FA'  and year(dtComprobante) = 2017 and month(dtComprobante) = 1
+
+	
+	select dbo.ufn_ValidarCUIT_v4_9_4 (nrDoc) as cuitok, 
+	tpIVA, nrDoc , vlTotalGeneral, dsRazonSocial,
+	nrTalonario, nrComprobante  ,   tpLetra , tpComprobante from TB_Comprobantes 
+	where dsRazonSocial like '%PETROTECNA%'
+
+
+
+	exec  [dbo].[spu_obtieneDatosCITIVentas_v4_9_4] @mes = 12 , @anio = 2016 , @renglon = 13129
+
+
+
+0008	00171595    	F	FA
+
+
+exec [dbo].[SP_ActualizarComprobante_v4_9]
+		@nrTalonario_param        = '0008',  /* FORMATO 0000*/
+		@nrComprobante_param      = '00171595',
+		@tpComprobante_param      = 'FA',
+		@tpLetra_param            = 'B',
+		@nrTalonario_new_param    = '0008',
+		@nrComprobante_new_param  = '00171595',
+		@tpComprobante_new_param  = 'FA',
+		@tpLetra_new_param        = 'B',
+		@solo_cambio_cabecera_comprobante = 'SI'
+
+------------------------------------------------------------------------------------------
+
+
+
+ exec  [dbo].[spu_obtieneDatosCITIVentas_v4_9_4] @mes = 12 , @anio = 2016 , @renglon = 23
+ 
+
+ exec  [dbo].[spu_obtieneDatosCITIVentas_v4_9_4] @mes = 12 , @anio = 2016 , @renglon = 1438
+ 
+
+ exec  [dbo].[spu_obtieneDatosCITIVentas_v4_9_4] @mes = 12 , @anio = 2016 , @renglon = 23
+ 
+
+ exec  [dbo].[spu_obtieneDatosCITIVentas_v4_9_4] @mes = 12 , @anio = 2016 , @renglon = 23
+ 
+
+ exec  [dbo].[spu_obtieneDatosCITIVentas_v4_9_4] @mes = 12 , @anio = 2016 , @renglon = 23
+
+
+ 
+ 
+	select * into zTB_Comprobantes_errores_originales_201701 from TB_Comprobantes 
+	where tpLetra  in ('A','M') and  tpComprobante = 'FA'  and year(dtComprobante) = 2016 and 
+	dbo.ufn_ValidarCUIT_v4_9_4 (nrDoc) = 0
+
+
+	select dbo.ufn_ValidarCUIT_v4_9_4 (nrDoc) as cuitok, 
+	tpIVA, nrDoc , vlTotalGeneral, dsRazonSocial,
+	nrTalonario, nrComprobante  ,   tpLetra , tpComprobante from TB_Comprobantes 
+	where tpLetra  in ('A','M') and  tpComprobante = 'FA'  and year(dtComprobante) = 2017 and month(dtComprobante) = 1
+
+
+		
+	select dbo.ufn_ValidarCUIT_v4_9_4 (nrDoc) as cuitok, 
+	tpIVA, nrDoc , vlTotalGeneral, dsRazonSocial,
+	nrTalonario, nrComprobante  ,   tpLetra , tpComprobante from TB_Comprobantes 
+	where dsRazonSocial like '%ANDREA CHERNIAVSKY%'
+
+
+	update x set  x.nrDoc = '27-13211971-1' from TB_Comprobantes x 
+where dsRazonSocial like '%ANDREA CHERNIAVSKY%'
+
+   	    		
+
+
+ 
+	select * into zTB_Comprobantes_errores_originales_201701 from TB_Comprobantes 
+	where tpLetra  in ('A','M') and  tpComprobante = 'FA'  and year(dtComprobante) = 2016 and 
+	dbo.ufn_ValidarCUIT_v4_9_4 (nrDoc) = 0
+
+
+	select dbo.ufn_ValidarCUIT_v4_9_4 (nrDoc) as cuitok, 
+	tpIVA, nrDoc , vlTotalGeneral, dsRazonSocial,
+	nrTalonario, nrComprobante  ,   tpLetra , tpComprobante from TB_Comprobantes 
+	where tpLetra  in ('A','M') and  tpComprobante = 'FA'  and year(dtComprobante) = 2017 
+	and month(dtComprobante) = 1
+
+
+	
+		
+	select dbo.ufn_ValidarCUIT_v4_9_4 (nrDoc) as cuitok, 
+	tpIVA, nrDoc , vlTotalGeneral, dsRazonSocial,
+	nrTalonario, nrComprobante  ,   tpLetra , tpComprobante from TB_Comprobantes 
+	where dsRazonSocial like '%ANDREA CHERNIAVSKY%'
+
+
+	 
+	select *  from TB_Comprobantes 
+	where tpLetra  in ('A','M') and  tpComprobante = 'FA'  and year(dtComprobante) = 2017 and 
+	dbo.ufn_ValidarCUIT_v4_9_4 (nrDoc) = 0 and month(dtComprobante) = 1
+	
+
+	update x set  x.nrDoc = '27-13211971-1' from TB_Comprobantes x 
+where dsRazonSocial like '%ANDREA CHERNIAVSKY%'
+
+
+
+	
+		
+	select dbo.ufn_ValidarCUIT_v4_9_4 (nrDoc) as cuitok, 
+	tpIVA, nrDoc , vlTotalGeneral, dsRazonSocial,
+	nrTalonario, nrComprobante  ,   tpLetra , tpComprobante from TB_Comprobantes 
+	where dsRazonSocial like '%RUBEN%'
+
+
+
+	
+ 
+	select * into zTB_Comprobantes_errores_originales_201702 from TB_Comprobantes 
+	where tpLetra  in ('A','M') and  tpComprobante = 'FA'  and year(dtComprobante) = 2017 and 
+	dbo.ufn_ValidarCUIT_v4_9_4 (nrDoc) = 0  and month(dtComprobante) = 2
+
+
+	
+	select dbo.ufn_ValidarCUIT_v4_9_4 (nrDoc) as cuitok, 
+	tpIVA, nrDoc , vlTotalGeneral, dsRazonSocial,
+	nrTalonario, nrComprobante  ,   tpLetra , tpComprobante from TB_Comprobantes 
+	where tpLetra  in ('A','M') and  tpComprobante = 'FA'  and year(dtComprobante) = 2017 
+	and month(dtComprobante) = 2
+
+
+	
+	select dbo.ufn_ValidarCUIT_v4_9_4 (nrDoc) as cuitok, 
+	tpIVA, nrDoc , vlTotalGeneral, dsRazonSocial,
+	nrTalonario, nrComprobante  ,   tpLetra , tpComprobante from TB_Comprobantes 
+	where dsRazonSocial like '%GONZALEZ SERGIO EDUARDO%'
+	
+	select *  from TB_Comprobantes 
+	where tpLetra  in ('A','M') and  tpComprobante = 'FA'  and year(dtComprobante) = 2017 and 
+	dbo.ufn_ValidarCUIT_v4_9_4 (nrDoc) = 0  and month(dtComprobante) = 2
