68c88e490af4c106c5e476c62b9e2ba842a0420b
Eulises Quidel <eulisesquidel@gmail.com>
2017-06-06 18:20:15 -0300

    incidente M601  preparando archivos v4.9.800

---
+++ Version4.9.800/ScriptBD_DML_MSSQL_v4.9.800.sql
@@ -0,0 +1,4 @@
+
+-- Cambios de DML version 4.9.800
+use dbSG2000
+go 
---
+++ incidentes/201706/analisis_comp_m601.sql
@@ -0,0 +1,18 @@
+select * from TB_Comprobantes  where tpComprobante = 'FA' and  nrComprobante = '00000601' and tpLetra = 'M'
+
+select * from TB_Recibos where  cdCliente = '4033'
+
+
+select * from TB_Recibos where  IdRecibo = 1000322910
+
+select * from TB_RecibosDetalle where  IdRecibo = 1000322910
+
+
+
+select * from TB_Cupones where IdReciboCtaCte = 1000322910
+
+select * from TB_Cupones where IdRecibo = 1000322910
+
+
+
+select 322910
