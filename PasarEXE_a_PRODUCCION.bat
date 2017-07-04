bf281886653e8305a932257307d751f51634aae8
Euli <eulisesquidel@hotmail.com>
2017-07-03 22:05:06 -0300

    03/07 agregado obs factura  v4.9.7x  archivo faltante

--- PasarEXE_a_PRODUCCION.bat
+++ PasarEXE_a_PRODUCCION.bat
@@ -1,10 +1,10 @@
-If  exist C:\Gestion\GestiondeViajesADM\Versiones_Anteriores\24062017\GestiondeViajes.exe  (
+If  exist C:\Gestion\GestiondeViajesADM\Versiones_Anteriores\03072017\GestiondeViajes.exe  (
 	xcopy GestiondeViajes.exe C:\Gestion\GestiondeViajesADM /y
 	xcopy GestiondeViajes.exe C:\Gestion\GestiondeViajesFacturacionRETORNO /y
 ) else (
-	MD C:\Gestion\GestiondeViajesADM\Versiones_Anteriores\24062017
-	xcopy C:\Gestion\GestiondeViajesADM\GestiondeViajes.exe C:\Gestion\GestiondeViajesADM\Versiones_Anteriores\24062017 /y
-	xcopy C:\Gestion\GestiondeViajesFacturacionRETORNO\GestiondeViajes.exe C:\Gestion\GestiondeViajesFacturacionRETORNO\Versiones_Anteriores\24062017  /y
+	MD C:\Gestion\GestiondeViajesADM\Versiones_Anteriores\03072017
+	xcopy C:\Gestion\GestiondeViajesADM\GestiondeViajes.exe C:\Gestion\GestiondeViajesADM\Versiones_Anteriores\03072017 /y
+	xcopy C:\Gestion\GestiondeViajesFacturacionRETORNO\GestiondeViajes.exe C:\Gestion\GestiondeViajesFacturacionRETORNO\Versiones_Anteriores\03072017  /y
 	xcopy GestiondeViajes.exe C:\Gestion\GestiondeViajesADM /y
 	xcopy GestiondeViajes.exe C:\Gestion\GestiondeViajesFacturacionRETORNO /y
 )
