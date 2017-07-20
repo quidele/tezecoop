dc9b751d10a2a7052a6a9e38a56c43ee58ac5448
Euli <eulisesquidel@hotmail.com>
2017-07-19 23:51:29 -0300

    19/07 citi ventas AFIP presentacion 03/2017 0.5h

--- incidentes/201707/citi_ventas/analisis.sql
+++ incidentes/201707/citi_ventas/analisis.sql
@@ -67,12 +67,15 @@ select * from tb_puestos
  
 
 
-  exec  [dbo].[spu_obtieneDatosCITIVentas_v4_9] @mes = 3 , @anio = 2017, @renglon = 1004
-
+  exec  [dbo].[spu_obtieneDatosCITIVentas_v4_9_4] @mes = 3 , @anio = 2017, @renglon = 1004
+  
 
 
 */
 
+
+
+select * from sys.procedures where name like   '%Alicuo%'
 
 CREATE procedure  [dbo].[spu_obtieneDatosCITIVentas_v4_9_4](@mes int, @anio int , @renglon int = -1) 
 as
@@ -211,20 +214,101 @@ GO
 		-- Debemos modificar el store de generacion 
 	select dbo.ufn_ValidarCUIT_v4_9_4 (nrDoc) as cuitok, 
 	tpIVA, nrDoc , vlTotalGeneral, dsRazonSocial,
-	nrTalonario, nrComprobante  ,   tpLetra , tpComprobante from TB_Comprobantes 
+	nrTalonario, nrComprobante  ,   tpLetra , tpComprobante 
+	 from TB_Comprobantes 
 	where  tpComprobante = 'FA'  and year(dtComprobante) = 2017 
 	and month(dtComprobante) = 3 and vlTotalGeneral > 999  and tpIVA = 'CF'
 
 
 
 --Agregamos campos para la nueva regneracion 	   
-if not exists (SELECT * FROM INFORMATION_SCHEMA.COLUMNS where TABLE_NAME='TB_Comprobantes' and COLUMN_NAME='dsRazonSocial_AFIP')
-     ALTER TABLE TB_Comprobantes ADD dsRazonSocial_AFIP VARCHAR(100) NULL
+if not exists (SELECT * FROM INFORMATION_SCHEMA.COLUMNS where TABLE_NAME='TB_Comprobantes' and COLUMN_NAME='dsRazonSocial_original')
+     ALTER TABLE TB_Comprobantes ADD dsRazonSocial_original VARCHAR(100) NULL
 
 	   
-if not exists (SELECT * FROM INFORMATION_SCHEMA.COLUMNS where TABLE_NAME='TB_Comprobantes' and COLUMN_NAME='nrDoc_AFIP')
-     ALTER TABLE TB_Comprobantes ADD nrDoc_AFIP CHAR(20) NULL
+if not exists (SELECT * FROM INFORMATION_SCHEMA.COLUMNS where TABLE_NAME='TB_Comprobantes' and COLUMN_NAME='nrDoc_original')
+     ALTER TABLE TB_Comprobantes ADD nrDoc_original CHAR(20) NULL
 
-select * from TB_Comprobantes
+
+
+	update x set x.dsRazonSocial_original = x.dsRazonSocial , x.nrDoc_original =  x.nrDoc
+	 from TB_Comprobantes  x 
+	where  tpComprobante = 'FA'  and year(dtComprobante) = 2017 
+	and month(dtComprobante) = 3 and vlTotalGeneral > 999  and tpIVA = 'CF'
+	and x.dsRazonSocial_original is null and  x.nrDoc_original  is null
+
+
+	update x set  x.dsRazonSocial = 'Consumidor Final' , x.nrDoc =  null
+	 from TB_Comprobantes  x 
+	where  tpComprobante = 'FA'  and year(dtComprobante) = 2017 
+	and month(dtComprobante) = 3 and vlTotalGeneral > 999  and tpIVA = 'CF'
+	and x.dsRazonSocial_original is null and  x.nrDoc_original  is null
+	and dbo.ufn_ValidarCUIT_v4_9_4 (nrDoc) = 0
+
 
 go 
+
+
+--Agregamos campos para la nueva regneracion 	   
+if not exists (SELECT * FROM INFORMATION_SCHEMA.COLUMNS where TABLE_NAME='TB_Comprobantes' and COLUMN_NAME='dsRazonSocial_original')
+     ALTER TABLE TB_Comprobantes ADD dsRazonSocial_original VARCHAR(100) NULL
+
+	   
+if not exists (SELECT * FROM INFORMATION_SCHEMA.COLUMNS where TABLE_NAME='TB_Comprobantes' and COLUMN_NAME='nrDoc_original')
+     ALTER TABLE TB_Comprobantes ADD nrDoc_original CHAR(20) NULL
+
+
+
+	 	select dbo.ufn_ValidarCUIT_v4_9_4 (nrDoc) as cuitok, 
+	tpIVA, nrDoc , vlTotalGeneral, dsRazonSocial,
+	nrTalonario, nrComprobante  ,   tpLetra , tpComprobante 
+	 from TB_Comprobantes 
+	where  tpComprobante = 'FA'  and year(dtComprobante) = 2017 
+	and month(dtComprobante) = 3 and vlTotalGeneral > 999  and tpIVA = 'CF'
+
+
+
+	
+	update x set x.dsRazonSocial_original = x.dsRazonSocial , x.nrDoc_original =  x.nrDoc
+	 from TB_Comprobantes  x 
+	where  tpComprobante = 'FA'  and year(dtComprobante) = 2017 
+	and month(dtComprobante) = 3 and vlTotalGeneral > 999  and tpIVA = 'CF'
+	and x.dsRazonSocial_original is null and  x.nrDoc_original  is null
+
+select * from TB_Comprobantes x  where 	
+ x.dsRazonSocial_original is not null or   x.nrDoc_original  is not null
+ and   tpComprobante = 'FA'  and year(dtComprobante) = 2017 
+	and month(dtComprobante) = 3 and vlTotalGeneral > 999  and tpIVA = 'CF'
+	
+	update x set  x.dsRazonSocial = 'Consumidor Final' , x.nrDoc =  null
+	 from TB_Comprobantes  x 
+	where  tpComprobante = 'FA'  and year(dtComprobante) = 2017 
+	and month(dtComprobante) = 3 and vlTotalGeneral > 999  and tpIVA = 'CF'
+	and dbo.ufn_ValidarCUIT_v4_9_4 (nrDoc) = 0
+
+
+
+	
+  exec  [dbo].[spu_obtieneDatosCITIVentas_v4_9_4] @mes = 3 , @anio = 2017
+
+
+  exec spu_obtieneDatosCITIVentas_Alicuotas_v4_9 @mes = 3 , @anio = 2017
+
+  
+
+
+select * from sys.procedures where name like   '%Alicuo%'
+
+
+  exec spu_obtieneDatosCITIVentas_Alicuotas_v4_9 @mes = 3 , @anio = 2017, @renglon = 1277
+ 
+  exec spu_obtieneDatosCITIVentas_Alicuotas_v4_9 @mes = 3 , @anio = 2017, @renglon = 1416
+
+  
+  exec spu_obtieneDatosCITIVentas_Alicuotas_v4_9 @mes = 3 , @anio = 2017, @renglon = 8496
+
+  
+  exec spu_obtieneDatosCITIVentas_Alicuotas_v4_9 @mes = 3 , @anio = 2017, @renglon = 8516
+
+  
+  exec spu_obtieneDatosCITIVentas_Alicuotas_v4_9 @mes = 3 , @anio = 2017, @renglon = 14747
