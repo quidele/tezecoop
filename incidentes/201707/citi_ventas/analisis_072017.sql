8e31d77fab3882102946b5d4112ad7c0e3a18406
Eulises Quidel <eulisesquidel@gmail.com>
2017-08-22 18:04:31 -0300

    archivos de la Prese AFIP 062017 y 072017 archivo faltante

--- incidentes/201707/citi_ventas/analisis_072017.sql
+++ incidentes/201707/citi_ventas/analisis_072017.sql
@@ -14,29 +14,27 @@
 	and month(dtComprobante) = 7 and vlTotalGeneral > 999  and tpIVA = 'CF'
 
 
-
-	
-	update x set x.dsRazonSocial_original = x.dsRazonSocial , x.nrDoc_original =  x.nrDoc
+select x.dsRazonSocial_original , x.nrDoc_original
 	 from TB_Comprobantes  x 
 	where  tpComprobante = 'FA'  and year(dtComprobante) = 2017 
-	and month(dtComprobante) = 7 and vlTotalGeneral > 999  and tpIVA = 'CF'
-	and x.dsRazonSocial_original is null and  x.nrDoc_original  is null
+	and month(dtComprobante) = 7 and vlTotalGeneral > 999  and tpIVA = 'CF' and
+	( x.dsRazonSocial_original is not  null or   x.nrDoc_original  is not null)
 
-	select * from TB_Comprobantes x  where 	
-	x.dsRazonSocial_original is not null or   x.nrDoc_original  is not null
-	and   tpComprobante = 'FA'  and year(dtComprobante) = 2017 
-	and month(dtComprobante) = 7 and vlTotalGeneral > 999  and tpIVA = 'CF'
-	
+	begin tran 
+
 	update x set  x.dsRazonSocial = 'Consumidor Final' , x.nrDoc =  null
 	 from TB_Comprobantes  x 
 	where  tpComprobante = 'FA'  and year(dtComprobante) = 2017 
-	and month(dtComprobante) = 3 and vlTotalGeneral > 999  and tpIVA = 'CF'
-	and dbo.ufn_ValidarCUIT_v4_9_4 (nrDoc) = 0
+	and month(dtComprobante) = 7 and vlTotalGeneral > 999  and tpIVA = 'CF'
+	
 
+	rollback tran 
+
+	commit tran 
 
 	
-  exec  [dbo].[spu_obtieneDatosCITIVentas_v4_9_4] @mes = 3 , @anio = 2017
+  exec  [dbo].[spu_obtieneDatosCITIVentas_v4_9_4] @mes = 7 , @anio = 2017
 
-  exec spu_obtieneDatosCITIVentas_Alicuotas_v4_9 @mes = 3 , @anio = 2017
+  exec spu_obtieneDatosCITIVentas_Alicuotas_v4_9 @mes = 7 , @anio = 2017
 
   
