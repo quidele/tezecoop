f1d1ea3eeb771445c8cb4f5b60e90684b4148f23
Eulises Quidel <eulisesquidel@gmail.com>
2017-09-12 18:06:19 -0300

    12/09/17 V4.9.XXX avance conci auto AMCA add serviceCupon 1.0h

---
+++ SGLibrary_COM/SGLibrary/Exceptions/DAOEntityException.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace SGLibrary.Exceptions
+{
+    public class DAOEntityException : Exception 
+    {
+        public DAOEntityException()
+        {
+
+        }
+
+        public DAOEntityException(String message)
+            : base(message)
+        {
+
+        }
+
+        public DAOEntityException(string message, Exception inner) 
+            : base(message, inner)
+        {
+        }
+
+
+    }
+}
