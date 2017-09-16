f800d6a8d05c531796bc94bc1517d0e8d6fd8622
Euli <eulisesquidel@hotmail.com>
2017-09-15 22:31:19 -0300

    15/09 V4.9.XXX creando ServiceConciliacionAutomaticaAMCA 0.5h

--- SGLibrary_COM/SGLibrary/ServiceConciliacionAutomaticaAMCA.cs
+++ SGLibrary_COM/SGLibrary/ServiceConciliacionAutomaticaAMCA.cs
@@ -13,181 +13,19 @@ using SGLibrary.Extensiones;
 
 namespace SGLibrary
 {
-    [Guid("65174DA4-2750-403E-A84F-AA685C296E26"),
-    ClassInterface(ClassInterfaceType.None),
-    ComSourceInterfaces(typeof(Conciliacion_Events))]
-    public class ServiceConciliacionAutomatica : ServiceConciliacion 
+
+
+    public class ServiceConciliacionAutomaticaAMCA : ServiceConciliacionAutomatica 
     {
 
-        public ServiceConciliacionAutomatica()
+        public ServiceConciliacionAutomaticaAMCA()
         {
 
         }
 
 
-        public void procesarArchivo(ArchivoTarjeta miArchivo)
-        {
 
-            miArchivo.ProcesarArchivo(); // procesa el archivo , sin importar el formato
-            Console.WriteLine(miArchivo.miArchivoTarjeta.formato + " " + miArchivo.miArchivoTarjeta.nombrearchivo);
-
-            //ServiceConciliacionAutomatica un_ServiceConciliacionAutomatica = new ServiceConciliacionAutomatica();
-            try
-            {
-            using (var context = new dbSG2000Entities())
-            {
-                using (TransactionScope transaction = new TransactionScope())
-                {
-                   
-                    var item = context.TB_ArchivoTarjeta.Add(miArchivo.miArchivoTarjeta);
-                    context.SaveChanges();
-                    transaction.Complete(); 
-                }
-
-            }
-          }
-         catch (DbEntityValidationException e)
-            {
-                foreach (var eve in e.EntityValidationErrors)
-            {
-                Console.WriteLine("Entity of type \"{0}\" in state \"{1}\" has the following validation errors:",
-                    eve.Entry.Entity.GetType().Name, eve.Entry.State);
-                foreach (var ve in eve.ValidationErrors)
-                {
-                    Console.WriteLine("- Property: \"{0}\", Error: \"{1}\"",
-                        ve.PropertyName, ve.ErrorMessage);
-                }
-            }
-            throw;
-            }
-
-       }
-
-
-        // Conciliar 
-        public void ConcilialiarAutomaticaticamente(TB_ArchivoTarjeta unTB_ArchivoTarjeta)
-        {
-
-            using (var context = new dbSG2000Entities())
-            {
-                try
-                {
-                // La logica de la conciliación queda suscripta en el SP
-                    spu_conciliarAutomaticamente_Result resul = context.spu_conciliarAutomaticamente(unTB_ArchivoTarjeta.id).First();
-                // verificar el resultado que devuelve el STORE
-                 Console.WriteLine(resul.resultado + " " + resul.descripcion_error);
-                }
-                catch (EntityCommandCompilationException e)
-                {
-                    Console.WriteLine( e.Message);
-                    throw;
-                }
-                
-            }
-          
-
-        } // FIN DE ConcilialiarAutomaticaticamente
-
-
-        public IEnumerable<Object> ObtenerViajesConciliadosAutomaticamente(Decimal  pIdArchivo)
-        {
-
-            using (var context = new dbSG2000Entities())
-            {
-                var listadeViajesaConciliar1 = (from c in context.TB_Cupones
-                                                 join x in context.TB_ArchivoTarjetaDetalle
-                                                    on c.nrCupon  equals x.nrCupon // Join entre las dos tablas
-                                                where (c.flCobradoalCliente == false) && (c.flCompensado == false)
-                                                && (c.flAnulado == false)
-                                                && (new[] { "Tarjeta de Crédito", "Tarjeta de Débito"}.Contains(c.tpCupon))
-                                                && (x.idarchivo == pIdArchivo) // filtramos por un archivo
-                                                select new
-                                                {
-                                                    ID = c.nrCupon,
-                                                    FECHA = c.dtCupon,
-                                                    LICENCIA = c.nrLicencia,
-                                                    DOC = c.tpComprobanteCliente,
-                                                    LETRA = c.tpLetraCliente,
-                                                    PDV = c.nrTalonarioCliente,
-                                                    NRO = c.nrComprabanteCliente,
-                                                    MONTO = c.vlMontoCupon,
-                                                    MONTO_ARCHI = x.importe,
-                                                    TARJETA = c.nrTarjeta,
-                                                    TARJETA_ARCHI = x.tarjeta,
-                                                    CUPON = c.nrCuponPosnet,
-                                                    CUPON_ARCHI = x.comprobante,
-                                                    NIVEL = x.nrNivelConciliacion,
-                                                    IdArchivoTarjetaDetalle = x.Id,
-                                                    FECHA_PAGO =  x.fechaPago.Value 
-                                                }).OrderBy(c => c.FECHA);
-
-                // 'nrDocTarjeta' , 'nrTarjeta' , 'tpDocTarjeta' 
-                Trace.TraceInformation(listadeViajesaConciliar1.ToString());
-                return listadeViajesaConciliar1.ToList();
-                //return listadeViajesaConciliar.ToList();
-
-            }
-
-
-        }
-
-        public IEnumerable<Object> ObtenerViajesNoConciliadosAutomaticamente(Decimal pIdArchivo)
-        {
-
-            using (var context = new dbSG2000Entities())
-            {
-                var listadeViajesaConciliar1 = (from x in context.TB_ArchivoTarjetaDetalle
-                                                where (x.idarchivo == pIdArchivo) // filtramos por un archivo
-                                                && (x.nrCupon == null)
-                                                select new
-                                                {
-                                                    ID = 0,
-                                                    FECHA = "",
-                                                    LICENCIA = "",
-                                                    DOC = "",
-                                                    LETRA = "",
-                                                    PDV = "",
-                                                    NRO = "",
-                                                    MONTO = "",
-                                                    MONTO_ARCHI = x.importe,
-                                                    TARJETA = "",
-                                                    TARJETA_ARCHI = x.tarjeta,
-                                                    CUPON = "",
-                                                    CUPON_ARCHI = x.comprobante,
-                                                    NIVEL = x.nrNivelConciliacion,
-                                                    IdArchivoTarjetaDetalle = x.Id,
-                                                    FECHA_PAGO = x.fechaPago.Value
-                                                });
-
-                // 'nrDocTarjeta' , 'nrTarjeta' , 'tpDocTarjeta' 
-                Trace.TraceInformation(listadeViajesaConciliar1.ToString());
-                return listadeViajesaConciliar1.ToList();
-                //return listadeViajesaConciliar.ToList();
-
-            }
-
-
-        }
-
-        public TB_ArchivoTarjeta obtenerArchivo(String pId)
-        {
-
-            Int32 id = Int32.Parse(pId);
-
-            using (var context = new dbSG2000Entities())
-            {
-                // Falta agregar filtro de fechas
-                TB_ArchivoTarjeta una_TB_ArchivoTarjeta = (from c in context.TB_ArchivoTarjeta
-                                                    where c.id == id
-                                                    select c)
-                                                 .First();
-                return una_TB_ArchivoTarjeta;
-            }
-        }
-
-
-
-        public virtual void agregarConciliacion(List<TB_ConciliacionDetalleEx> plistaDetalleConciliacion, TB_Conciliacion objConciliacion)
+        public override void agregarConciliacion(List<TB_ConciliacionDetalleEx> plistaDetalleConciliacion, TB_Conciliacion objConciliacion)
         {
             ServiceMovimientoContable unSMC = new ServiceMovimientoContable();
 
@@ -270,65 +108,8 @@ namespace SGLibrary
         //// fin agregar conciliacion
 
 
-        /// <summary>
-        /// 
-        /// </summary>
-        /// <param name="una_conciliacion"></param>
-        /// <returns></returns>
-        public  IEnumerable<Object>  ObtenerDetalleConciliacionAutomatica(long pId)
-        {
-            List<Decimal> ids_cupones = new List<Decimal>();
 
-            //una_conciliacion.TB_ConciliacionDetalle 
-            using (var context = new dbSG2000Entities())
-            {
-
-                // Falta agregar filtro de fechas
-                var una_conciliacionDetalle = (from c in context.TB_ConciliacionDetalle
-                                               where c.IdConciliacion == pId
-                                               select c);
-
-                foreach (var item in una_conciliacionDetalle)
-                {
-                    ids_cupones.Add(item.nrCupon);
-                }
-
-                var listadeViajesaConciliar1 = (from c in context.TB_Cupones 
-                                                    join x in una_conciliacionDetalle on c.nrCupon equals x.nrCupon
-                                                    join y in context.TB_ArchivoTarjetaDetalle on x.IdArchivoTarjetaDetalle equals y.Id
-                                                where ids_cupones.Contains(c.nrCupon)
-                                                select new
-                                                {
-                                                    ID = c.nrCupon,
-                                                    FECHA = c.dtCupon,
-                                                    LICENCIA = c.nrLicencia,
-                                                    DOC = c.tpComprobanteCliente,
-                                                    LETRA = c.tpLetraCliente,
-                                                    PDV = c.nrTalonarioCliente,
-                                                    NRO = c.nrComprabanteCliente,
-                                                    MONTO = c.vlMontoCupon,
-                                                    MONTO_ARCHI = y.importe,
-                                                    TARJETA = c.nrTarjeta,
-                                                    TARJETA_ARCHI = y.tarjeta,
-                                                    CUPON = c.nrCuponPosnet,
-                                                    CUPON_ARCHI = y.comprobante,
-                                                    NIVEL = y.nrNivelConciliacion,
-                                                    IdArchivoTarjetaDetalle = y.Id,
-                                                    FECHA_PAGO =  x.fechaPago.Value,
-                                                    COMPENSADO = c.flCompensado == true ? "SI" : "NO"
-                                                });
-
-                // 'nrDocTarjeta' , 'nrTarjeta' , 'tpDocTarjeta' 
-
-                return listadeViajesaConciliar1.ToList();
-                //return listadeViajesaConciliar.ToList();
-
-            }
-        }
-
-
-
-        public virtual void modificarConciliacionAutomatica(List<decimal> listaCuponesDesconciliados, 
+        public override void modificarConciliacionAutomatica(List<decimal> listaCuponesDesconciliados, 
                                                       List<decimal> listaCuponesConciliados,
                                                       TB_Conciliacion objConciliacion)
         {
@@ -416,7 +197,7 @@ namespace SGLibrary
         }
 
 
-        public virtual void anularConciliacionAutomatica(TB_Conciliacion objConciliacion)
+        public override void anularConciliacionAutomatica(TB_Conciliacion objConciliacion)
         {
             ServiceMovimientoContable unSMC = new ServiceMovimientoContable();
 
