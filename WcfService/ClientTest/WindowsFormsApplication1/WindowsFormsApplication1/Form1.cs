a8bc1e6ad475be2fc58648208109726851576a76
Euli <eulisesquidel@hotmail.com>
2017-12-03 09:55:57 -0300

    03/11 i+d wcf

---
+++ WcfService/ClientTest/WindowsFormsApplication1/WindowsFormsApplication1/Form1.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using WindowsFormsApplication1.ServiceReference1;
+
+namespace WindowsFormsApplication1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Service1Client client = new Service1Client();
+
+            // Use the 'client' variable to call operations on the service.
+            MessageBox.Show ( client.obtenerRegistros().valor.ToString()  ) ;
+            // Always close the client.
+            client.Close();
+
+
+        }
+    }
+}
