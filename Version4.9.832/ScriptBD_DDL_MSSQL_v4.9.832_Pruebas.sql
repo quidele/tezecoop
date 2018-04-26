e5a82e0105e581d0645ab45fc65fe0ff7b70fb5e
Euli <eulisesquidel@hotmail.com>
2018-04-26 09:41:35 -0300

    26/04 Corrigiendo Bugs Obligaciones en VB6 1.0

--- Version4.9.832/ScriptBD_DDL_MSSQL_v4.9.832_Pruebas.sql
+++ Version4.9.832/ScriptBD_DDL_MSSQL_v4.9.832_Pruebas.sql
@@ -467,3 +467,17 @@ declare @flAnulado     bit
 END
 
 GO
+
+
+
+ALTER VIEW dbo.VW_DetalleMovimientosContablesCupones
+AS
+SELECT DISTINCT 
+                      dbo.TB_MovimientosContables.tpOperacion, dbo.TB_MovimientosContables.nrCaja, dbo.TB_MovimientosContables.dsConcepto, 
+                      dbo.TB_Cupones.nrLicencia, dbo.TB_Cupones.tpCupon, dbo.TB_Cupones.nrTalonarioCliente, dbo.TB_Cupones.nrComprabanteCliente, 
+                      dbo.TB_Cupones.vlPagoPesos AS vlPesos, dbo.TB_Cupones.vlPagoDolares AS vlDolares, dbo.TB_Cupones.vlPagoEuros AS vlEuros, 
+                      dbo.TB_Cupones.vlComision, dbo.TB_Cupones.vlMontoCupon, dbo.TB_MovimientosContables.nrRecibo, dbo.TB_Cupones.flCompensado, 
+                      dbo.TB_Cupones.dtCompensado, dbo.TB_MovimientosContables.dsUsuario, dbo.TB_Cupones.vlIVA
+FROM         dbo.TB_MovimientosContables INNER JOIN
+                      dbo.TB_Cupones ON dbo.TB_MovimientosContables.IdRecibo = dbo.TB_Cupones.IdRecibo
+					  WHERE  dbo.TB_Cupones.tpCupon not in ('Débito')
