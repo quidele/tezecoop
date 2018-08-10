7b353b4333bf3ee91791a09deee4053654a907fd
Eulises Quidel <eulisesquidel@gmail.com>
2018-08-10 18:12:36 -0300

    08/08  analisis BUG RrcargoTarjeta 08-2018 0.5h

---
+++ incidentes/2018/201808/analisis_dif_recergaTarjeta.sql
@@ -0,0 +1,13 @@
+
+select dtComprobante , nrPuesto , vlTotalGeneral , vlRecargoTarjeta, vlIVA , *  from TB_Comprobantes  where nrTalonario = '0007' and nrComprobante = '00610663'
+select dtComprobante , nrPuesto , vlTotalGeneral , vlRecargoTarjeta, vlIVA , *  from TB_Comprobantes  where nrTalonario = '0006' and nrComprobante = '00829636'
+
+select round(104.4545,2)
+
+
+select *  from TB_ComprobantesDetalle  where nrTalonario = '0007' and nrComprobante = '00610663'
+select *  from TB_ComprobantesDetalle  where nrTalonario = '0006' and nrComprobante = '00829636'
+select *  from TB_Comprobantes  where nrTalonario = '0007' and nrComprobante = '00610663'
+select *  from TB_Comprobantes  where nrTalonario = '0006' and nrComprobante = '00829636'
+
+select  * from TB_Puestos
