ce39c9c8ace8b039814d8c5852ee3fdee817ff02
Euli <eulisesquidel@hotmail.com>
2018-08-20 23:56:16 -0300

    archivos faltantes

--- Version4.9.834b/ScriptBD_DDL_MSSQL_Pruebas_v4.9.834b.sql
+++ Version4.9.834b/ScriptBD_DDL_MSSQL_Pruebas_v4.9.834b.sql
@@ -2,3 +2,20 @@
 -- Cambios de DDL version 4.9.834
 use dbSG2000_Pruebas
 go
+
+-- Agregamos el campo nro_trans en  TB_Comprobantes
+IF NOT  EXISTS  (SELECT   o.Name, c.Name FROM     sys.columns c  JOIN sys.objects o ON o.object_id = c.object_id 
+								WHERE    o.type = 'U'   and o.Name = 'TB_Comprobantes'  and  c.Name = 'nrCAE' )
+	ALTER TABLE dbo.TB_Comprobantes ADD  nrCAE varchar(15)
+
+GO
+
+
+-- Agregamos el campo nro_trans en  TB_Comprobantes
+IF NOT  EXISTS  (SELECT   o.Name, c.Name FROM     sys.columns c  JOIN sys.objects o ON o.object_id = c.object_id 
+								WHERE    o.type = 'U'   and o.Name = 'TB_Comprobantes'  and  c.Name = 'dtVencimientoCAE' )
+	ALTER TABLE dbo.TB_Comprobantes ADD  dtVencimientoCAE varchar(15)
+
+
+GO
+
--- Version4.9.834b/ScriptBD_DML_MSSQL_Pruebas_v4.9.834b.sql
+++ Version4.9.834b/ScriptBD_DML_MSSQL_Pruebas_v4.9.834b.sql
@@ -2,3 +2,18 @@
 -- Cambios de DML version 4.9.834
 use dbSG2000_Pruebas
 go 
+
+
+
+
+DELETE FROM DiccionariodeDatos WHERE nmTabla='TB_Comprobantes' and nmCampo = 'nrCAE' 
+
+INSERT INTO diccionariodedatos (nmTabla, nmCampo, nmCampoExterno, tpTipo, nrTamanio, flClave, flClaveForanea, flBusqueda, nrOrdenBusqueda, vlPuesto1, vlPuesto2, vlPuesto3, vlPuesto9, Actualizar, vlPuesto4, vlPuesto5) VALUES 
+  ('TB_Comprobantes','nrCAE','nrCAE','DECIMAL','15',NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL);
+
+
+  
+DELETE FROM DiccionariodeDatos WHERE nmTabla='TB_Comprobantes' and nmCampo = 'dtVencimientoCAE' 
+
+INSERT INTO diccionariodedatos (nmTabla, nmCampo, nmCampoExterno, tpTipo, nrTamanio, flClave, flClaveForanea, flBusqueda, nrOrdenBusqueda, vlPuesto1, vlPuesto2, vlPuesto3, vlPuesto9, Actualizar, vlPuesto4, vlPuesto5) VALUES 
+  ('TB_Comprobantes','dtVencimientoCAE','dtVencimientoCAE','SMALLDATETIME','15',NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL);
